ea3cb4a7bca9a658e42b382ba3104062ebbf7943
pab <devnull@localhost>
2009-02-06 22:32:54 +0000

    Add utility to write pre-built schema files

---
+++ storewxs.py
@@ -0,0 +1,25 @@
+import PyWXSB.XMLSchema as xs
+
+import sys
+import traceback
+from xml.dom import minidom
+from xml.dom import Node
+import pickle
+
+files = sys.argv[1:]
+if 0 == len(files):
+    files = [ 'schemas/xml.xsd', 'xml.wxs' ]
+
+xsd_file = files[0]
+wxs_file = files[1]
+
+wxs = xs.schema()
+try:
+    wxs.processDocument(minidom.parse(xsd_file))
+    ns = wxs.getTargetNamespace()
+    print 'Storing %s in %s' % (ns.uri(), wxs_file)
+    ns.saveToFile(wxs_file)
+except Exception, e:
+    sys.stderr.write("%s processing %s:\n" % (e.__class__, xsd_file))
+    traceback.print_exception(*sys.exc_info())
+
