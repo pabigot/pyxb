3281916cafef95144de970ada00a94bd90121e34
pab <devnull@localhost>
2009-02-06 22:31:08 +0000

    Add two more schema

--- schemas/getxsd.sh
+++ schemas/getxsd.sh
@@ -6,3 +6,5 @@ test -f httpbind.xsd || wget -O httpbind.xsd http://schemas.xmlsoap.org/wsdl/htt
 test -f mimebind.xsd || wget -O mimebind.xsd http://schemas.xmlsoap.org/wsdl/mime/
 test -f XMLSchema.xsd || wget http://www.w3.org/2001/XMLSchema.xsd
 test -f xml.xsd || wget http://www.w3.org/2001/xml.xsd
+test -f XMLSchema-hasFacetAndProperty.xsd || wget -O XMLSchema-hasFacetAndProperty.xsd http://www.w3.org/2001/XMLSchema-hasFacetAndProperty
+test -f xhtml.xsd || wget http://www.w3.org/1999/xhtml.xsd
