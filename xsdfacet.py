7454db77827a7df1e2568b22e36883b71c70a3b0
pab <devnull@localhost>
2009-03-06 21:05:48 +0000

    Cleanup, remove restriction to builtins

--- xsdfacet.py
+++ xsdfacet.py
@@ -13,6 +13,8 @@ Namespace.XMLSchema.modulePath(None)
 def facetLiteral (value):
     if isinstance(value, xs.datatypes._PST_mixin):
         return value.pythonLiteral()
+    if isinstance(value, type) and issubclass(value, xs.datatypes._PST_mixin):
+        return '%s %s' % (value.__module__, value.__name__)
     if isinstance(value, xs.facets.Facet):
         #return '%s.XsdSuperType()._CF_%s' % (value.ownerTypeDefinition().ncName(), value.Name())
         return '%s._CF_%s' % (value.ownerTypeDefinition().ncName(), value.Name())
@@ -26,11 +28,15 @@ def facetLiteral (value):
         return facetLiteral(value.value)
     if isinstance(value, xs.structures.SimpleTypeDefinition):
         return value.ncName()
-    print type(value)
+    print 'Unexpected literal type %s' % (type(value),)
     return str(value)
 
+files = sys.argv[1:]
+if 0 == len(files):
+    files = [ 'schemas/XMLSchema.xsd' ]
+
 try:
-    wxs = xs.schema().CreateFromDOM(minidom.parse('schemas/XMLSchema.xsd'))
+    wxs = xs.schema().CreateFromDOM(minidom.parse(files[0]))
     ns = wxs.getTargetNamespace()
 
     type_defs = ns.typeDefinitions()
@@ -40,16 +46,18 @@ try:
         for td in type_defs:
             if not isinstance(td, xs.structures.SimpleTypeDefinition):
                 continue
-            if not td.isBuiltin():
+            if td.targetNamespace() != ns:
+                continue
+            if (Namespace.XMLSchema == td.targetNamespace()) and (not td.isBuiltin()):
                 continue
             dep_types = td.dependentTypeDefinitions()
             ready = True
             for dtd in dep_types:
+                if dtd.targetNamespace() != ns:
+                    continue
                 if dtd == td:
-                    print 'Direct loop for %s' % (td,)
                     continue
                 if not (dtd in emit_order):
-                    print '%s fails due to %s' % (td, dtd)
                     ready = False
                     break
             if ready:
@@ -58,34 +66,39 @@ try:
                 new_type_defs.append(td)
         type_defs = new_type_defs
 
-    outf = file('datatypesi.py', 'w')
-    outf.write("from datatypes import *\n")
+    outf = file('datatypesi.tmp', 'w')
+    outf.write('''
+import facets
+from datatypes import *
+''')
 
     for td in emit_order:
-        if not isinstance(td, xs.structures.SimpleTypeDefinition):
-            continue
-        if td.isBuiltin():
-            for (fc, fi) in td.facets().items():
-                if (fi is None) and (fc in td.baseTypeDefinition().facets()):
-                    # Nothing new here
-                    continue
-                if (fi is not None) and (fi.ownerTypeDefinition() != td):
-                    # Did this one in an ancestor
-                    continue
-                argset = { }
-                is_collection = issubclass(fc, xs.facets._CollectionFacet_mixin)
-                if issubclass(fc, xs.facets._LateDatatype_mixin):
-                    argset['value_datatype'] = td.baseTypeDefinition()
-                if fi is not None:
-                    if not is_collection:
-                        argset['value'] = fi.value()
-                    if fi.superFacet() is not None:
-                        argset['super_facet'] = fi.superFacet()
-                facet_var = '%s._CF_%s' % (td.ncName(), fc.Name())
-                outf.write("%s = facets.CF_%s(%s)\n" % (facet_var, fc.Name(), ', '.join([ '%s=%s' % (key, facetLiteral(val)) for (key, val) in argset.items() ])))
-                if (fi is not None) and is_collection:
-                    for i in fi.items():
-                        outf.write("%s.setFromKeywords(value=%s)\n" % (facet_var, facetLiteral(i)))
+        #print 'Emitting %d facets in %s' % (len(td.facets()), td)
+        for (fc, fi) in td.facets().items():
+            if (fi is None) and (fc in td.baseTypeDefinition().facets()):
+                # Nothing new here
+                #print 'No instance'
+                continue
+            if (fi is not None) and (fi.ownerTypeDefinition() != td):
+                # Did this one in an ancestor
+                #print 'Parent instance'
+                continue
+            argset = { }
+            is_collection = issubclass(fc, xs.facets._CollectionFacet_mixin)
+            if issubclass(fc, xs.facets._LateDatatype_mixin):
+                argset['value_datatype'] = fc.BindingValueDatatype(td)
+            if fi is not None:
+                if not is_collection:
+                    argset['value'] = fi.value()
+                if fi.superFacet() is not None:
+                    argset['super_facet'] = fi.superFacet()
+            facet_var = '%s._CF_%s' % (td.ncName(), fc.Name())
+            outf.write("%s = facets.CF_%s(%s)\n" % (facet_var, fc.Name(), ', '.join([ '%s=%s' % (key, facetLiteral(val)) for (key, val) in argset.items() ])))
+            if (fi is not None) and is_collection:
+                for i in fi.items():
+                    argset = { }
+                    if isinstance(i, xs.facets._EnumerationElement):
+                        outf.write("%s.%s%s = %s._CF_enumeration.addKeyword(tag=%s)\n" % (td.ncName(), 'EV_', i.value, td.ncName(), facetLiteral(i.value)))
 
 except Exception, e:
     sys.stderr.write("%s processing %s:\n" % (e.__class__, file))
