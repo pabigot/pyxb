2b44ef54bcd70760a271b1557da35bbf798e6c30
pab <devnull@localhost>
2009-03-07 22:55:21 +0000

    Rework trying to generalize the generator

--- xsdfacet.py
+++ xsdfacet.py
@@ -28,58 +28,94 @@ def PrefixModule (value, text=None):
     if text is None:
         text = value.__name__
     if value.__module__ == xs.datatypes.__name__:
-        text = 'datatypes.%s' % (text,)
-    elif value.__module__ == xs.facets.__name__:
-        text = 'facets.%s' % (text,)
-    else:
-        assert False
-    return text
+        return 'datatypes.%s' % (text,)
+    if value.__module__ == xs.facets.__name__:
+        return 'facets.%s' % (text,)
+    raise IncompleteImplementationError('PrefixModule needs support for non-builtin instances')
 
 class ReferenceLiteral (object):
-    typeDefinition = None
+
+    # Either a STD or a subclass of _Enumeration_mixin, this is the
+    # class in which the referenced object is a member.
+    __ownerClass = None
     def __init__ (self, **kw):
-        self.typeDefinition = kw['type_definition']
+        self.__ownerClass = kw.get('type_definition', None)
 
     def _addTypePrefix (self, text):
-        if self.typeDefinition is not None:
-            text = '%s.%s' % (facetLiteral(self.typeDefinition), text)
+        if self.__ownerClass is not None:
+            text = '%s.%s' % (pythonLiteral(self.__ownerClass), text)
         return text
 
 class ReferenceFacetMember (ReferenceLiteral):
-    typeDefinition = None
-    facetClass = None
+    __facetClass = None
 
     def __init__ (self, **kw):
         super(ReferenceFacetMember, self).__init__(**kw)
-        self.facetClass = kw.get('facet_class', None)
+        self.__facetClass = kw['facet_class']
 
     def asLiteral (self, **kw):
-        return self._addTypePrefix('_CF_%s' % (self.facetClass.Name(),))
+        return self._addTypePrefix('_CF_%s' % (self.__facetClass.Name(),))
 
 class ReferenceEnumerationMember (ReferenceLiteral):
-    typeDefinition = None
-    facetInstance = None
     enumerationElement = None
     
     def __init__ (self, **kw):
-        super(ReferenceEnumerationMember, self).__init__(**kw)
-        self.facetInstance = kw.get('facet_instance', None)
+        # NB: Pre-extended __init__
+        
+        # All we really need is the enumeration element, so we can get
+        # its tag, and a type definition or datatype, so we can create
+        # the proper prefix.
+
+        # See if we were given a value, from which we can extract the
+        # other information.
+        value = kw.get('value', None)
+        assert (value is None) or isinstance(value, xs.facets._Enumeration_mixin)
+
+        # Must provide facet_instance, or a value from which it can be
+        # obtained.
+        facet_instance = kw.get('facet_instance', None)
+        if facet_instance is None:
+            assert isinstance(value, xs.facets._Enumeration_mixin)
+            facet_instance = value._CF_enumeration
+        assert isinstance(facet_instance, xs.facets.CF_enumeration)
+
+        # Must provide the enumeration_element, or a facet_instance
+        # and value from which it can be identified.
         self.enumerationElement = kw.get('enumeration_element', None)
+        if self.enumerationElement is None:
+            assert value is not None
+            self.enumerationElement = facet_instance.elementForValue(value)
+        assert isinstance(self.enumerationElement, xs.facets._EnumerationElement)
+
+        # If no type definition was provided, use the value datatype
+        # for the facet.
+        kw.setdefault('type_definition', facet_instance.valueDatatype())
+
+        super(ReferenceEnumerationMember, self).__init__(**kw)
 
     def asLiteral (self, **kw):
-        return self._addTypePrefix('%s_%s' % (self.facetInstance.enumPrefix(), self.enumerationElement.tag))
+        return self._addTypePrefix(self.enumerationElement.tag())
 
-
-def facetLiteral (value):
-    global TargetNamespace
+def pythonLiteral (value):
+    # For dictionaries, apply translation to all values (not keys)
     if isinstance(value, types.DictionaryType):
-        return ', '.join([ '%s=%s' % (k, facetLiteral(v)) for (k, v) in value.items() ])
+        return ', '.join([ '%s=%s' % (k, pythonLiteral(v)) for (k, v) in value.items() ])
+
+    # For tuples, apply translation to all members
     if isinstance(value, types.TupleType):
-        return tuple([ facetLiteral(_v) for _v in value ])
+        return tuple([ pythonLiteral(_v) for _v in value ])
+
+    # Value is a binding value for which there should be an
+    # enumeration constant.  Return that constant.
     if isinstance(value, xs.facets._Enumeration_mixin):
-        return PrefixModule(value, value._CF_enumeration.tagForValue(value))
+        return pythonLiteral(ReferenceEnumerationMember(value=value))
+
+    # Value is an instance of a Python binding, e.g. one of the
+    # XMLSchema datatypes.  Use its value, applying the proper prefix
+    # for the module.
     if isinstance(value, xs.datatypes._PST_mixin):
         return PrefixModule(value, value.pythonLiteral())
+
     if isinstance(value, type):
         if issubclass(value, xs.datatypes._PST_mixin):
             return PrefixModule(value)
@@ -91,9 +127,9 @@ def facetLiteral (value):
     if isinstance(value, types.StringTypes):
         return utility.QuotedEscaped(value,)
     if isinstance(value, xs.facets._PatternElement):
-        return facetLiteral(value.pattern)
+        return pythonLiteral(value.pattern)
     if isinstance(value, xs.facets._EnumerationElement):
-        return facetLiteral(value.value)
+        return pythonLiteral(value.value())
     if isinstance(value, xs.structures.SimpleTypeDefinition):
         return PrefixNamespace(value.targetNamespace(), value.ncName())
     if isinstance(value, ReferenceLiteral):
@@ -163,10 +199,9 @@ import %sdatatypes as datatypes
             argset = { }
             is_collection = issubclass(fc, xs.facets._CollectionFacet_mixin)
             if issubclass(fc, xs.facets._LateDatatype_mixin):
+                vdt = td
                 if fc.LateDatatypeBindsSuperclass():
-                    vdt = td.baseTypeDefinition()
-                else:
-                    vdt = td
+                    vdt = vdt.baseTypeDefinition()
                 argset['value_datatype'] = vdt
             if fi is not None:
                 if not is_collection:
@@ -176,14 +211,14 @@ import %sdatatypes as datatypes
                 if isinstance(fi, xs.facets.CF_enumeration):
                     argset['enum_prefix'] = fi.enumPrefix()
             facet_var = ReferenceFacetMember(type_definition=td, facet_class=fc)
-            outf.write("%s = %s(%s)\n" % facetLiteral( (facet_var, fc, argset )))
+            outf.write("%s = %s(%s)\n" % pythonLiteral( (facet_var, fc, argset )))
             if (fi is not None) and is_collection:
                 for i in fi.items():
                     if isinstance(i, xs.facets._EnumerationElement):
                         enum_member = ReferenceEnumerationMember(type_definition=td, facet_instance=fi, enumeration_element=i)
-                        outf.write("%s = %s.addKeyword(unicode_value=%s)\n" % facetLiteral( (enum_member, facet_var, i.unicodeValue )))
+                        outf.write("%s = %s.addKeyword(unicode_value=%s)\n" % pythonLiteral( (enum_member, facet_var, i.unicodeValue() )))
                     if isinstance(i, xs.facets._PatternElement):
-                        outf.write("%s.addPattern(pattern=%s)\n" % facetLiteral( (facet_var, i.pattern )))
+                        outf.write("%s.addPattern(pattern=%s)\n" % pythonLiteral( (facet_var, i.pattern )))
 
 except Exception, e:
     sys.stderr.write("%s processing %s:\n" % (e.__class__, file))
