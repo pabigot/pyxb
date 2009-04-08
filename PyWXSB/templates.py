cf3c17649bbb7ade0e14c7780b1017db7a29cefe
pab <devnull@localhost>
2009-04-08 01:08:37 +0000

    Try to improve comments

--- PyWXSB/templates.py
+++ PyWXSB/templates.py
@@ -1,23 +1,46 @@
 import re
 import unittest
 
+# POSIX shell variable syntax:
+#  Expansions with unset var
+#  ${var}=
+#  ${var+WORD}=
+#  ${var:+WORD}=
+#  ${var-WORD}=WORD
+#  ${var:-WORD}=WORD
+#  Expansions with empty var
+#  ${var}=
+#  ${var+WORD}=WORD
+#  ${var:+WORD}=
+#  ${var-WORD}=
+#  ${var:-WORD}=WORD
+#  Expansions with var=SET
+#  ${var}=SET
+#  ${var+WORD}=WORD
+#  ${var:+WORD}=WORD
+#  ${var-WORD}=SET
+#  ${var:-WORD}=SET
+
 # This expression replaces markers in template text with the value
 # obtained by looking up the marker in a dictionary.
+# %{id} = value
 _substIdPattern = re.compile("%{(?P<id>\w+)}")
 
 # This expression performs conditional substitution: if the expression
 # provided evaluates to true in a given context, then one value is
 # substituted, otherwise the alternative value is substituted.
+# %{?<cond>??<true>?:<false>?}
 # %{?1 == 2??true?:false?}
 _substConditionalPattern = re.compile("%{\?(?P<expr>.+?)\?\?(?P<true>.*?)(\?:(?P<false>.*?))?\?}", re.MULTILINE + re.DOTALL)
 
 # This expression tests whether an identifier is defined to a non-None
 # value in the context; if so, it replaces the marker with template
 # text.  In that replacement text, the value ?@ is replaced by the
-# test expression.
+# test expression.  Contrast POSIX shell ${ID+subst}${ID-subst}
 # Note: NOT by the value of the test expression.  If no replacement
 # text is given, the replacement '%{?@}' is used, which replaces it
 # with the value of the test expression.
+# %{?<id>?+<yessubst>?-?<nosubst>}}
 # %{?maybe_text?+?@ is defined to be %{?@}?}
 _substIfDefinedPattern = re.compile("%{\?(?P<id>\w+)(\?\+(?P<repl>.*?))?(\?\-(?P<ndrepl>.*?))?\?}", re.MULTILINE + re.DOTALL)
 
