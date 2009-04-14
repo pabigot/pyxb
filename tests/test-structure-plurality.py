b303017747c81d19ab7a6dcd34fd918d472bb2d0
pab <devnull@localhost>
2009-04-14 11:45:24 +0000

    Particle of sequence modelgroups

--- tests/test-structure-plurality.py
+++ tests/test-structure-plurality.py
@@ -4,6 +4,7 @@
 import PyWXSB.XMLSchema as xs
 from PyWXSB.exceptions_ import *
 import PyWXSB.Namespace as Namespace
+from PyWXSB.XMLSchema.structures import ModelGroup
 
 import unittest
 
@@ -29,10 +30,10 @@ class _TestBase (unittest.TestCase):
                  xs.structures.Particle(self.schema().lookupElement('ielt'), max_occurs=None, schema=self.schema())]
 
     def _getMGSingle (self, compositor):
-        return xs.structures.ModelGroup(compositor=compositor, particles=self._getSingleElements(), schema=self.schema())
+        return ModelGroup(compositor=compositor, particles=self._getSingleElements(), schema=self.schema())
 
     def _getMGMulti (self, compositor):
-        return xs.structures.ModelGroup(compositor=compositor, particles=self._getMultiElements(), schema=self.schema())
+        return ModelGroup(compositor=compositor, particles=self._getMultiElements(), schema=self.schema())
 
 class TestED (_TestBase):
     def testBasic (self):
@@ -48,7 +49,7 @@ class TestED (_TestBase):
 
 class TestMG (_TestBase):
     def testSequenceSingleElements (self):
-        mgd = self._getMGSingle(xs.structures.ModelGroup.C_SEQUENCE)
+        mgd = self._getMGSingle(ModelGroup.C_SEQUENCE)
         pd = mgd.pluralityData()
         self.assertEqual(1, len(pd))
         pde = pd[0]
@@ -57,7 +58,7 @@ class TestMG (_TestBase):
         self.assert_( ('ielt', 1) in pde )
 
     def testSequenceMultiElements (self):
-        mgd = self._getMGMulti(xs.structures.ModelGroup.C_SEQUENCE)
+        mgd = self._getMGMulti(ModelGroup.C_SEQUENCE)
         pd = mgd.pluralityData()
         self.assertEqual(1, len(pd))
         pde = pd[0]
@@ -66,7 +67,7 @@ class TestMG (_TestBase):
         self.assert_( ('ielt', None) in pde )
 
     def testAllSingleElements (self):
-        mgd = self._getMGSingle(xs.structures.ModelGroup.C_ALL)
+        mgd = self._getMGSingle(ModelGroup.C_ALL)
         pd = mgd.pluralityData()
         self.assertEqual(1, len(pd))
         pde = pd[0]
@@ -75,7 +76,7 @@ class TestMG (_TestBase):
         self.assert_( ('ielt', 1) in pde )
 
     def testAllMultiElements (self):
-        mgd = self._getMGMulti(xs.structures.ModelGroup.C_ALL)
+        mgd = self._getMGMulti(ModelGroup.C_ALL)
         pd = mgd.pluralityData()
         self.assertEqual(1, len(pd))
         pde = pd[0]
@@ -84,7 +85,7 @@ class TestMG (_TestBase):
         self.assert_( ('ielt', None) in pde )
 
     def testChoiceSingleElements (self):
-        mgd = self._getMGSingle(xs.structures.ModelGroup.C_CHOICE)
+        mgd = self._getMGSingle(ModelGroup.C_CHOICE)
         pd = mgd.pluralityData()
         self.assertEqual(2, len(pd))
         pde = pd[0]
@@ -95,7 +96,7 @@ class TestMG (_TestBase):
         self.assert_( ('ielt', 1) in pde )
 
     def testChoiceMultiElements (self):
-        mgd = self._getMGMulti(xs.structures.ModelGroup.C_CHOICE)
+        mgd = self._getMGMulti(ModelGroup.C_CHOICE)
         pd = mgd.pluralityData()
         self.assertEqual(2, len(pd))
         pde = pd[0]
@@ -149,6 +150,34 @@ class TestParticle (_TestBase):
         (name, count) = pde[0]
         self.assertEqual(ed.ncName(), name)
         self.assertEqual(None, count)
+
+    def testOptionalMGSeq (self):
+        prt = xs.structures.Particle(self._getMGMulti(ModelGroup.C_SEQUENCE), min_occurs=0, max_occurs=1, schema=self.schema())
+        pd = prt.pluralityData()
+        self.assertEqual(1, len(pd))
+        pde = pd[0]
+        self.assertEqual(2, len(pde))
+        self.assert_( ('selt', 4) in pde )
+        self.assert_( ('ielt', None) in pde )
+        
+    def testMultipleMGSeq (self):
+        prt = xs.structures.Particle(self._getMGMulti(ModelGroup.C_SEQUENCE), min_occurs=3, max_occurs=3, schema=self.schema())
+        pd = prt.pluralityData()
+        self.assertEqual(1, len(pd))
+        pde = pd[0]
+        self.assertEqual(2, len(pde))
+        self.assert_( ('selt', 12) in pde )
+        self.assert_( ('ielt', None) in pde )
+        
+    def testUnboundedMGSeq (self):
+        ed = self.schema().lookupElement('selt')
+        prt = xs.structures.Particle(self._getMGMulti(ModelGroup.C_SEQUENCE), min_occurs=3, max_occurs=None, schema=self.schema())
+        pd = prt.pluralityData()
+        self.assertEqual(1, len(pd))
+        pde = pd[0]
+        self.assertEqual(2, len(pde))
+        self.assert_( ('selt', None) in pde )
+        self.assert_( ('ielt', None) in pde )
 
 if __name__ == '__main__':
     unittest.main()
