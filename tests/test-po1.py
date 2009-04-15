9df8645818bb09876666c9272855f37f5845b2d8
pab <devnull@localhost>
2009-04-15 13:51:05 +0000

    Augmented testing

--- tests/test-po1.py
+++ tests/test-po1.py
@@ -11,23 +11,35 @@ from PyWXSB.exceptions_ import *
 import unittest
 
 class TestPO1 (unittest.TestCase):
-    street_xml = '''<street>95 Main St.
-Anytown, AS  12345-6789</street>'''
+    street_content = '''95 Main St.
+Anytown, AS  12345-6789'''
+    street_xml = '<street>%s</street>' % (street_content,)
     street_dom = minidom.parseString(street_xml).documentElement
 
-    def testElementSimpleContent (self):
+    # Note: name comes before street in the schema.  For now, the
+    # order maintenance is wrong.
+    #address1_xml = '<name>Customer</name><street>95 Main St</street>'
+    #address2_xml = '<name>Sugar Mama</name><street>24 E. Dearling Ave.</street>'
+    address1_xml = '<street>95 Main St</street><name>Customer</name>'
+    address2_xml = '<street>24 E. Dearling Ave.</street><name>Sugar Mama</name>'
+
+    def testPythonElementSimpleContent (self):
+        elt = USAddress_street(self.street_content)
+        self.assertEqual(self.street_content, elt.content())
+        self.assertEqual(elt.toDOM().toxml(), self.street_xml)
+
+    def testDOMElementSimpleContent (self):
         elt = USAddress_street.CreateFromDOM(self.street_dom)
         self.assertEqual(elt.toDOM().toxml(), self.street_xml)
 
-    def testUSAddress (self):
-        #str = USAddress_street('1595 fernwood')
-        #print str
-        xml = '<shipTo><street>95 Main St</street><name>Customer</name></shipTo>'
+    def testPythonElementComplexContent_Element (self):
         addr = USAddress(name='Customer', street='95 Main St')
+        self.assertEqual('95 Main St', addr.street().content())
+        self.assertEqual('<s>%s</s>' % (self.address1_xml,), addr.toDOM(tag='s').toxml())
+
+        #xml = '<shipTo>%s</shipTo>' % (self.address1_xml,)
         #print addr.toDOM(tag='shipTo').toxml()
-        #self.assertEqual('95 Main St', addr.street().content())
-        #self.assertEqual(xml, addr.toDOM(tag='shipTo').toxml())
-        dom = minidom.parseString(xml)
+        #dom = minidom.parseString(xml)
         #addr2 = USAddress.CreateFromDOM(dom)
         #self.assertEqual(xml, addr2.toDOM(tag='shipTo').toxml())
 
