00aa8bc02a6ca0665e51a7ba0f83a0ee44c72285
pab <devnull@localhost>
2009-04-16 13:41:58 +0000

    Start preparing for distribution

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+# Require Python 2.4
+
+setup(name='PyWXSB',
+      description'Python W3C Schema XML Bindings',
+      author='Peter A. Bigot',
+      author_email='pywxsb@comcast.net',
+      url='http://www.comcast.net/~pabigot/pywxsb',
+      version='0.1.1',
+      packages=['PyWXSB'])
+      
