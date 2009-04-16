49edb873507bf8a2ecf1b93e4f2883707d6e32b5
pab <devnull@localhost>
2009-04-16 22:43:19 +0000

    Start infrastructure for wildcard support

---
+++ tests/drivers/test-wildcard.py
@@ -0,0 +1,25 @@
+import pywxsb.binding.generate
+from xml.dom import minidom
+from xml.dom import Node
+
+import os.path
+schema_path = '%s/../schemas/test-wildcard.xsd' % (os.path.dirname(__file__),)
+code = pywxsb.binding.generate.GeneratePython(schema_file=schema_path)
+
+rv = compile(code, 'test', 'exec')
+eval(rv)
+
+from pywxsb.exceptions_ import *
+
+import unittest
+
+class TestWildcard (unittest.TestCase):
+    def testBasic (self):
+        xml = '<wrapper><first/><second/><third/></wrapper>'
+        doc = minidom.parseString(xml)
+        instance = wrapper.CreateFromDOM(doc.documentElement)
+
+if __name__ == '__main__':
+    unittest.main()
+    
+        
