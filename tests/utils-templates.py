56ff70fd3065fe7b6cc6dfcf01bb66a81a0578a3
pab <devnull@localhost>
2009-04-16 15:35:33 +0000

    Move utility unit tests into standard location

---
+++ tests/utils-templates.py
@@ -0,0 +1,92 @@
+import unittest
+from pywxsb.utils.templates import *
+
+_dictionary = { 'one' : 'un'
+              , 'two' : 'dau'
+              , 'three' : 'tri'
+              , 'un' : 1
+              , 'dau': 2
+              , 'tri': 3
+              , 'empty': ''
+              , 'defined': 'value'
+              }
+
+class IfDefinedPatternTestCase (unittest.TestCase):
+    dictionary = _dictionary
+
+    def testNoSubst (self):
+        self.assertEquals('un', replaceInText('%{?one?}', **self.dictionary))
+        self.assertEquals('', replaceInText('%{?four?}', **self.dictionary))
+
+    def testPlainSubst (self):
+        self.assertEquals('one is defined', replaceInText('%{?one?+one is defined?}', **self.dictionary))
+        self.assertEquals('', replaceInText('%{?four?+four is defined?}', **self.dictionary))
+        self.assertEquals('four is not defined', replaceInText('%{?four?+@? is defined?-?@ is not defined?}', **self.dictionary))
+        self.assertEquals('name', replaceInText('%{?context?+%{?@}.?}name'))
+        self.assertEquals('name', replaceInText('%{?context?+%{?@}.?}name', context=None))
+        self.assertEquals('owner.name', replaceInText('%{?context?+%{?@}.?}name', context='owner'))
+
+    def testWithSubst (self):
+        self.assertEquals('un and un are two', replaceInText('%{?one?+%{?@} and %{?@} are two?-what is ?@??}', **self.dictionary))
+        self.assertEquals('what is "four"?', replaceInText('%{?four?+%{?@} and %{?@} are two?-what is "?@"??}', **self.dictionary))
+
+
+class ConditionalPatternTestCase (unittest.TestCase):
+    dictionary = _dictionary
+    
+    def testBasic (self):
+        self.assertEquals('three is defined', replaceInText('%{?three??three is defined?:three is not defined?}', **self.dictionary))
+        self.assertEquals("%{EXCEPTION: name 'four' is not defined}", replaceInText('%{?four??four is defined?:four is not defined?}', **self.dictionary))
+        self.assertEquals('value', replaceInText('%{?defined??%{defined}?:pass?}', **self.dictionary))
+        self.assertEquals('pass', replaceInText('%{?empty??%{empty}?:pass?}', **self.dictionary))
+
+    def testHalfExpressions (self):
+        self.assertEquals('value is three', replaceInText('%{?3 == un+dau??value is three?}', **self.dictionary))
+        self.assertEquals('', replaceInText('%{?3 == un-dau??value is three?}', **self.dictionary))
+        self.assertEquals('good 1 == un', replaceInText('%{?1 == un??good ?@?:bad ?@?}', **self.dictionary))
+        self.assertEquals('bad 2 == un', replaceInText('%{?2 == un??good ?@?:bad ?@?}', **self.dictionary))
+        self.assertEquals('''
+        if runtime_test:
+            print 'Good on 1 == un'
+''', replaceInText('''
+        %{?1 == un??if runtime_test:
+            print 'Good on ?@'
+?}''', **self.dictionary))
+
+    def testExpressions (self):
+        self.assertEquals('value is three', replaceInText('%{?3 == un+dau??value is three?:value is not three?}', **self.dictionary))
+        self.assertEquals('value is not three', replaceInText('%{?3 == un-dau??value is three?:value is not three?}', **self.dictionary))
+
+    def testNesting (self):
+        self.assertEquals('tri', replaceInText('%{?3 == un+dau??%{three}?:not %{three}?}', **self.dictionary))
+
+class IdPatternTestCase (unittest.TestCase):
+    dictionary = _dictionary
+
+    def testNoChange (self):
+        cases = [ 'plain text'
+                , '   leading whitespace'
+                , 'trailing whitespace '
+                , '''Multiline
+text''' ] # '''
+        for c in cases:
+            self.assertEquals(c, replaceInText(c, **{}))
+
+    def testSimpleSubstitution (self):
+        self.assertEquals('un', replaceInText('%{one}', **self.dictionary))
+        self.assertEquals('un and dau', replaceInText('%{one} and %{two}', **self.dictionary))
+        self.assertEquals('''Line un
+Line dau
+Line tri
+''', replaceInText('''Line %{one}
+Line %{two}
+Line %{three}
+''', **self.dictionary))
+
+    def testMissing (self):
+        self.assertEquals('%{MISSING:four}', replaceInText('%{four}', **self.dictionary))
+
+if __name__ == '__main__':
+    #print replaceInText('%{?three%?three is defined%:three is not defined?}', ConditionalPatternTestCase.dictionary)
+    #print "\n".join(_substConditionalPattern.match('%{?three%?three is defined%:three is not defined?}').groups())
+    unittest.main()
---
+++ tests/utils-utility.py
@@ -0,0 +1,98 @@
+import unittest
+from pywxsb.utils.utility import *
+
+class DST_base (_DeconflictSymbols_mixin):
+    _ReservedSymbols = set([ 'one', 'two' ])
+
+class DST_sub (DST_base):
+    _ReservedSymbols = DST_base._ReservedSymbols.union(set([ 'three' ]))
+
+class DeconfictSymbolsTtest (unittest.TestCase):
+    def testDeconflict (self):
+        self.assertEquals(2, len(DST_base._ReservedSymbols))
+        self.assertEquals(3, len(DST_sub._ReservedSymbols))
+
+class BasicTest (unittest.TestCase):
+    
+    cases = ( ( r'"1\x042"', "1\0042" ) # expanded octal
+            , ( r'"1\x042"', '1\0042' ) # expanded octal (single quotes do not affect escaping)
+            , ( "r'1\\0042'", r'1\0042' ) # preserve unexpanded octal
+            , ( r'"1\x222&3"', '1"2&3' )  # escape double quotes
+            , ( '"one\'two"', "one'two" ) # preserve single quote
+            , ( r'"1\n2"', "1\n2" )       # expanded newline to escape sequence
+            , ( "r'1\\n2'", r'1\n2' )     # raw backslash preserved
+            , ( "\"1'\\n'2\"", "1'\n'2" ) # expanded newline to escape sequence
+            , ( "\"1'\\n'2\"", '1\'\n\'2' ) # expanded newline to escape sequence (single quotes)
+            , ( "\"1\\x22\\n\\x222\"", '1"\n"2' ) # escape double quotes around expanded newline
+            , ( "r'1\\'\\n\\'2'", r'1\'\n\'2' )   # preserve escaped quote and newline
+            , ( r'u"1\u00042"', u"1\0042" )       # unicode expanded octal
+            , ( r'u"1\u00222&3"', u'1"2&3' )      # unicode escape double quotes
+            , ( r'u"one' + "'" + r'two"', u"one'two" ) # unicode embedded single quote
+            , ( "r'\\i\\c*'", r'\i\c*' )               # backslashes as in patterns
+            , ( u'u"0"', u'\u0030' )                   # expanded unicode works
+            , ( u'u"\\u0022"', u'"' )      # unicode double quotes are escaped
+            , ( u'u"\\u0022"', u'\u0022' ) # single quotes don't change that expanded unicode works
+            , ( u'u"\\u0022"', ur'\u0022' ) # raw has no effect on unicode escapes
+            , ( u"u\"'\"", u"'" )           # unicode single quote works
+            , ( u"u\"\\u00220\\u0022\"", u'"\u0030"' ) # unicode with double quotes works
+            )
+            
+
+    def testPrepareIdentifier (self):
+        in_use = set()
+        self.assertEquals('id', PrepareIdentifier('id', in_use))
+        self.assertEquals('id_', PrepareIdentifier('id', in_use))
+        self.assertEquals('id_2', PrepareIdentifier('id_', in_use))
+        self.assertEquals('id_3', PrepareIdentifier('id____', in_use))
+        self.assertEquals('_id', PrepareIdentifier('id', in_use, protected=True))
+        self.assertEquals('_id_', PrepareIdentifier('id', in_use, protected=True))
+        self.assertEquals('__id', PrepareIdentifier('id', in_use, private=True))
+        self.assertEquals('__id_', PrepareIdentifier('id', in_use, private=True))
+
+        reserved = frozenset([ 'Factory' ])
+        in_use = set()
+        self.assertEquals('Factory_', PrepareIdentifier('Factory', in_use, reserved))
+        self.assertEquals('Factory__', PrepareIdentifier('Factory', in_use, reserved))
+        self.assertEquals('Factory__2', PrepareIdentifier('Factory', in_use, reserved))
+
+        in_use = set()
+        self.assertEquals('global_', PrepareIdentifier('global', in_use))
+        self.assertEquals('global__', PrepareIdentifier('global', in_use))
+        self.assertEquals('global__2', PrepareIdentifier('global', in_use))
+
+
+    def testQuotedEscape (self):
+        for ( expected, input ) in self.cases:
+            result = QuotedEscaped(input)
+            # Given "expected" value may not be correct.  Don't care as
+            # long as the evalution produces the input.
+            #self.assertEquals(expected, result)
+            self.assertEquals(input, eval(result))
+
+    def testMakeIdentifier (self):
+        self.assertEquals('id', MakeIdentifier('id'))
+        self.assertEquals('id', MakeIdentifier(u'id'))
+        self.assertEquals('id_sep', MakeIdentifier(u'id_sep'))
+        self.assertEquals('id_sep', MakeIdentifier(u'id sep'))
+        self.assertEquals('id_sep_too', MakeIdentifier(u'id-sep too'))
+        self.assertEquals('idid', MakeIdentifier(u'id&id'))
+        self.assertEquals('id', MakeIdentifier('_id'))
+        self.assertEquals('id_', MakeIdentifier('_id_'))
+
+    def testDeconflictKeyword (self):
+        self.assertEquals('id', DeconflictKeyword('id'))
+        self.assertEquals('for_', DeconflictKeyword('for'))
+
+    def testMakeUnique (self):
+        in_use = set()
+        self.assertEquals('id', MakeUnique('id', in_use))
+        self.assertEquals(1, len(in_use))
+        self.assertEquals('id_', MakeUnique('id', in_use))
+        self.assertEquals(2, len(in_use))
+        self.assertEquals('id_2', MakeUnique('id', in_use))
+        self.assertEquals(3, len(in_use))
+        self.assertEquals(set(( 'id', 'id_', 'id_2' )), in_use)
+
+if '__main__' == __name__:
+    unittest.main()
+    
