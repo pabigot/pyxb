d8fb41a55e423c0dbed1b48f5d273172f2636bda
pab <devnull@localhost>
2009-04-20 00:59:08 +0000

    Add and rearrange for distribution builds

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ setup(name='PyWXSB',
       description = 'Python W3C Schema XML Bindings',
       author='Peter A. Bigot',
       author_email='pywxsb@comcast.net',
-      url='http://www.comcast.net/~pabigot/pywxsb',
+      url='http://home.comcast.net/~pabigot/pywxsb',
       version='0.1.1',
-      package_dir = { '': 'src' },
-      packages=[ 'pywxsb', 'pywxsb.binding', 'pywxsb.utils', 'pywxsb.xmlschema' ])
+      packages=[ 'pywxsb', 'pywxsb.binding', 'pywxsb.utils', 'pywxsb.xmlschema', 'pywxsb.standard.bindings' ],
+      data_files= [ ('pywxsb/standard/schemas', [ '*.xsd' ] ) ],
+      scripts=[ 'scripts/genbind' ])
       
