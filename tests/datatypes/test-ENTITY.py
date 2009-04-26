7416f7658ba196c6cd8d7d311a1c5007f3cd778e
pab <devnull@localhost>
2009-04-26 19:36:49 +0000

    Implement.

--- tests/datatypes/test-ENTITY.py
+++ tests/datatypes/test-ENTITY.py
@@ -1,9 +1,19 @@
+from pyxb.exceptions_ import *
 import unittest
 import pyxb.binding.datatypes as xsd
 
 class Test_ENTITY (unittest.TestCase):
-    def testRange (self):
-        self.assertFalse("Datatype ENTITY test not implemented")
+    def testValid (self):
+        valid = [ 'schema', '_Underscore', '_With.Dot', 'With-Hyphen' ]
+        for f in valid:
+            self.assertEqual(f, xsd.ENTITY(f))
+
+    def testInvalid (self):
+        invalid = [ '.DotFirst', 'With Spaces', 'With:Colon', 
+                    '  LeadingSpace', 'TrailingSpace  ',
+                    'With?Illegal', '??LeadingIllegal', 'TrailingIllegal??']
+        for f in invalid:
+            self.assertRaises(BadTypeValueError, xsd.ENTITY, f)
 
 if __name__ == '__main__':
     unittest.main()
