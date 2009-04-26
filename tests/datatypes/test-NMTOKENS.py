fa4f926021c94a1e7e8ffc8b448d6fd2801457c7
pab <devnull@localhost>
2009-04-26 20:13:15 +0000

    Implement

--- tests/datatypes/test-NMTOKENS.py
+++ tests/datatypes/test-NMTOKENS.py
@@ -1,9 +1,20 @@
+from pyxb.exceptions_ import *
 import unittest
 import pyxb.binding.datatypes as xsd
 
 class Test_NMTOKENS (unittest.TestCase):
-    def testRange (self):
-        self.assertFalse("Datatype NMTOKENS test not implemented")
+    def testBasicLists (self):
+        v = xsd.NMTOKENS([ "one", "_two", "three" ])
+        self.assertEqual(3, len(v))
+        self.assertTrue(isinstance(v[0], xsd.NMTOKEN))
+        self.assertEqual("one", v[0])
+
+    def testStringLists (self):
+        v = xsd.NMTOKENS("one _two three")
+        self.assertEqual(3, len(v))
+        self.assertEqual("one", v[0])
+        self.assertTrue(isinstance(v[0], xsd.NMTOKEN))
+        self.assertRaises(BadTypeValueError, xsd.NMTOKENS, 'string with b@d id')
 
 if __name__ == '__main__':
     unittest.main()
