158bba112616eec2702e6772e6cd2cc1e43eb38c
pab <devnull@localhost>
2009-04-26 22:50:01 +0000

    Fix missed instances of old name

--- maintainer/dobinds.sh
+++ maintainer/dobinds.sh
@@ -1,20 +1,20 @@
 mkdir -p bindings
-rm pywxsb/standard/bindings/[a-z]*
+rm pyxb/standard/bindings/[a-z]*
 
 # Some must be done in order, to handle dependencies
 # NOTE: Use prefix xml_ instead of xml because xml conflicts with standard package
-scripts/genbind pywxsb/standard/schemas/xml.xsd pywxsb/standard/bindings xml_
+scripts/genbind pyxb/standard/schemas/xml.xsd pyxb/standard/bindings xml_
 # NOTE: XSD depends on XML which depends on XSD.  Since we have a workable
 # XSD schema built-in, do not create an external pre-loaded schema for
 # which we'll have to resolve in XML and subsequently in XSD.
-#scripts/genbind pywxsb/standard/schemas/XMLSchema.xsd pywxsb/standard/bindings xs
-scripts/genbind pywxsb/standard/schemas/XMLSchema-hasFacetAndProperty.xsd pywxsb/standard/bindings xsd_hfp
-scripts/genbind pywxsb/standard/schemas/wsdl.xsd pywxsb/standard/bindings wsdl
-scripts/genbind pywxsb/standard/schemas/xhtml.xsd pywxsb/standard/bindings xhtml
+#scripts/genbind pyxb/standard/schemas/XMLSchema.xsd pyxb/standard/bindings xs
+scripts/genbind pyxb/standard/schemas/XMLSchema-hasFacetAndProperty.xsd pyxb/standard/bindings xsd_hfp
+scripts/genbind pyxb/standard/schemas/wsdl.xsd pyxb/standard/bindings wsdl
+scripts/genbind pyxb/standard/schemas/xhtml.xsd pyxb/standard/bindings xhtml
 
 # Process anything remaining
-scripts/genbind pywxsb/standard/schemas/kml21.xsd pywxsb/standard/bindings kml
-scripts/genbind pywxsb/standard/schemas/xmldsig-core-schema.xsd pywxsb/standard/bindings xmldsig
-scripts/genbind pywxsb/standard/schemas/xenc-schema.xsd pywxsb/standard/bindings xenc
+scripts/genbind pyxb/standard/schemas/kml21.xsd pyxb/standard/bindings kml
+scripts/genbind pyxb/standard/schemas/xmldsig-core-schema.xsd pyxb/standard/bindings xmldsig
+scripts/genbind pyxb/standard/schemas/xenc-schema.xsd pyxb/standard/bindings xenc
 
-scripts/genbind pywxsb/standard/schemas/saml-schema-assertion-2.0.xsd pywxsb/standard/bindings saml_assert
+scripts/genbind pyxb/standard/schemas/saml-schema-assertion-2.0.xsd pyxb/standard/bindings saml_assert
--- play.py
+++ play.py
@@ -1,9 +1,9 @@
 import sys
-import pywxsb.binding.generate
+import pyxb.binding.generate
 
 files = sys.argv[1:]
 if 0 == len(files):
     files = [ 'schemas/kml21.xsd' ]
 
-rv = pywxsb.binding.generate.GeneratePython(schema_file=files[0])
+rv = pyxb.binding.generate.GeneratePython(schema_file=files[0])
 print rv
