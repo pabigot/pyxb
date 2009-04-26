b793bcd9a60a097d5b7da40ec60b5c9e7fd74496
pab <devnull@localhost>
2009-04-26 16:29:16 +0000

    More rename stuff

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 # Require Python 2.4
 
-setup(name='PyWXSB',
-      description = 'Python W3C Schema XML Bindings',
+setup(name='PyXB',
+      description = 'Python W3C XML Schema Bindings',
       author='Peter A. Bigot',
-      author_email='pywxsb@comcast.net',
-      url='http://home.comcast.net/~pabigot/pywxsb',
+      author_email='pyxb@comcast.net',
+      url='http://home.comcast.net/~pyxb',
       version='0.1.3',
-      packages=[ 'pywxsb', 'pywxsb.binding', 'pywxsb.utils', 'pywxsb.xmlschema', 'pywxsb.standard.bindings' ],
-      data_files= [ ('pywxsb/standard/schemas', [ '*.xsd' ] ) ],
+      packages=[ 'pyxb', 'pyxb.binding', 'pyxb.utils', 'pyxb.xmlschema', 'pyxb.standard.bindings' ],
+      data_files= [ ('pyxb/standard/schemas', [ '*.xsd' ] ) ],
       scripts=[ 'scripts/genbind' ])
       
