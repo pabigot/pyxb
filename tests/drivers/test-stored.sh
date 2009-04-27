afaab578043b76ce3421789af1f81c9bd3664415
pab <devnull@localhost>
2009-04-27 01:39:03 +0000

    Go on to attempt to load the second binding

--- tests/drivers/test-stored.sh
+++ tests/drivers/test-stored.sh
@@ -2,6 +2,16 @@ PYTHONPATH=../..
 rm -rf bindings
 mkdir bindings
 PYXB_NAMESPACE_PATH='bindings:../../pyxb/standard/bindings'
+export PYXB_NAMESPACE_PATH
 
 ../../scripts/genbind ../schemas/shared-types.xsd bindings st
 ../../scripts/genbind ../schemas/test-external.xsd bindings te
+OFN=test-stored-$$.py
+cat >>${OFN} <<EOText
+import pyxb.Namespace
+print "\n".join(pyxb.Namespace.AvailableForLoad())
+ns = pyxb.Namespace.NamespaceForURI('URN:test-external', True)
+ns.validateSchema()
+EOText
+python ${OFN}
+rm -f ${OFN}
