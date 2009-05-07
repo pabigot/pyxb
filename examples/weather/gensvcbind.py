4ad06836e009309c7b5c00f0f932f9db38dff15c
pab <devnull@localhost>
2009-05-07 21:26:18 +0000

    Update how schema is built

--- examples/weather/gensvcbind.py
+++ examples/weather/gensvcbind.py
@@ -3,7 +3,6 @@ import pyxb.xmlschema as xs
 import sys
 
 import pyxb.standard.bindings.wsdl as wsdl
-import pyxb.standard.bindings.soaphttp as soaphttp
 from xml.dom import Node
 from xml.dom import minidom
 import pyxb.binding.generate
@@ -14,17 +13,10 @@ root = doc.documentElement
 
 attribute_map = domutils.AttributeMap(root)
 
-spec = wsdl.definitions.CreateFromDOM(doc.documentElement)
+try:
+    spec = wsdl.definitions.CreateFromDOM(doc.documentElement, process_schema=True)
+except Exception, e:
+    print 'ERROR building schema: %s' % (e,)
+    sys.exit(1)
 
-print pyxb.Namespace.AvailableForLoad()
-
-for t in spec.types():
-    for t2 in t.wildcardElements():
-        if isinstance(t2, Node):
-            print t2.namespaceURI
-            attribute_map.update(domutils.AttributeMap(t2))
-            ns = pyxb.Namespace.NamespaceForURI(t2.namespaceURI)
-            type_spec = xs.schema.CreateFromDOM(t2, attribute_map)
-            print type_spec
-            open('weather.py', 'w').write(pyxb.binding.generate.GeneratePython(schema=type_spec))
-            sys.exit(0)
+open('raw_weather.py', 'w').write(pyxb.binding.generate.GeneratePython(schema=spec.schema()))
