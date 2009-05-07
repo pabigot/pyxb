383dceeb1864b14446e975e9c3f5a42d0cc55396
pab <devnull@localhost>
2009-05-07 18:02:12 +0000

    Rename to match standard prefixes

--- maintainer/dobinds.sh
+++ maintainer/dobinds.sh
@@ -6,17 +6,17 @@ scripts/genbind pyxb/standard/schemas/xml.xsd pyxb/standard/bindings/raw xml_
 # NOTE: XSD depends on XML which depends on XSD.  Since we have a workable
 # XSD schema built-in, do not create an external pre-loaded schema for
 # which we'll have to resolve in XML and subsequently in XSD.
-#scripts/genbind pyxb/standard/schemas/XMLSchema.xsd pyxb/standard/bindings/raw xs
+#scripts/genbind pyxb/standard/schemas/XMLSchema.xsd pyxb/standard/bindings/raw xsd
 scripts/genbind pyxb/standard/schemas/XMLSchema-hasFacetAndProperty.xsd pyxb/standard/bindings/raw xsd_hfp
 scripts/genbind pyxb/standard/schemas/wsdl.xsd pyxb/standard/bindings/raw wsdl
 scripts/genbind pyxb/standard/schemas/xhtml.xsd pyxb/standard/bindings/raw xhtml
 
 # Useful wsdl dependents
-scripts/genbind pyxb/standard/schemas/soap.xsd pyxb/standard/bindings/raw soap
+scripts/genbind pyxb/standard/schemas/envsoap.xsd pyxb/standard/bindings/raw soapenv
 scripts/genbind pyxb/standard/schemas/soapenc.xsd pyxb/standard/bindings/raw soapenc
-scripts/genbind pyxb/standard/schemas/mimebind.xsd pyxb/standard/bindings/raw soapmime
-scripts/genbind pyxb/standard/schemas/soapbind.xsd pyxb/standard/bindings/raw soapbind
-scripts/genbind pyxb/standard/schemas/httpbind.xsd pyxb/standard/bindings/raw soaphttp
+scripts/genbind pyxb/standard/schemas/mime.xsd pyxb/standard/bindings/raw mime
+scripts/genbind pyxb/standard/schemas/soap.xsd pyxb/standard/bindings/raw soap
+scripts/genbind pyxb/standard/schemas/http.xsd pyxb/standard/bindings/raw http
 scripts/genbind pyxb/standard/schemas/soap12.xsd pyxb/standard/bindings/raw soap12
 
 # Process anything remaining
--- maintainer/getxsd.sh
+++ maintainer/getxsd.sh
@@ -1,10 +1,10 @@
-test -f soap.xsd || wget -O soap.xsd http://schemas.xmlsoap.org/soap/envelope/
+test -f soapenv.xsd || wget -O soapenv.xsd http://schemas.xmlsoap.org/soap/envelope/
 test -f soap12.xsd || wget -O soap12.xsd http://schemas.xmlsoap.org/wsdl/soap12/wsdl11soap12.xsd
 test -f soapenc.xsd || wget -O soapenc.xsd http://schemas.xmlsoap.org/soap/encoding/
 test -f wsdl.xsd || wget -O wsdl.xsd http://schemas.xmlsoap.org/wsdl/
-test -f soapbind.xsd || wget -O soapbind.xsd http://schemas.xmlsoap.org/wsdl/soap/
-test -f httpbind.xsd || wget -O httpbind.xsd http://schemas.xmlsoap.org/wsdl/http/
-test -f mimebind.xsd || wget -O mimebind.xsd http://schemas.xmlsoap.org/wsdl/mime/
+test -f soap.xsd || wget -O soap.xsd http://schemas.xmlsoap.org/wsdl/soap/
+test -f http.xsd || wget -O http.xsd http://schemas.xmlsoap.org/wsdl/http/
+test -f mime.xsd || wget -O mime.xsd http://schemas.xmlsoap.org/wsdl/mime/
 test -f XMLSchema.xsd || wget http://www.w3.org/2001/XMLSchema.xsd
 test -f xml.xsd || wget http://www.w3.org/2001/xml.xsd
 test -f XMLSchema-hasFacetAndProperty.xsd || wget -O XMLSchema-hasFacetAndProperty.xsd http://www.w3.org/2001/XMLSchema-hasFacetAndProperty
