f071b8276a746f3750850529c11ddc1cd0fb496f
pab <devnull@localhost>
2009-05-07 03:25:18 +0000

    Add specialized wsdl module

--- examples/weather/gensvcbind.py
+++ examples/weather/gensvcbind.py
@@ -1,18 +1,18 @@
 import pyxb.Namespace
 import pyxb.xmlschema as xs
+import sys
 
 import pyxb.standard.bindings.wsdl as wsdl
 import pyxb.standard.bindings.soaphttp as soaphttp
 from xml.dom import Node
 from xml.dom import minidom
 import pyxb.binding.generate
+import pyxb.utils.domutils as domutils
 
 doc = minidom.parse('weather.wsdl')
 root = doc.documentElement
 
-#for ai in range(0, root.attributes.length):
-#    attr = root.attributes.item(ai)
-#    print '%s = %s [%s]' % (attr.name, attr.value, attr.namespaceURI)
+attribute_map = domutils.AttributeMap(root)
 
 spec = wsdl.definitions.CreateFromDOM(doc.documentElement)
 
@@ -22,8 +22,9 @@ for t in spec.types():
     for t2 in t.wildcardElements():
         if isinstance(t2, Node):
             print t2.namespaceURI
+            attribute_map.update(domutils.AttributeMap(t2))
             ns = pyxb.Namespace.NamespaceForURI(t2.namespaceURI)
-            type_spec = xs.schema().CreateFromDOM(t2, root.attributes)
+            type_spec = xs.schema.CreateFromDOM(t2, attribute_map)
             print type_spec
             open('weather.py', 'w').write(pyxb.binding.generate.GeneratePython(schema=type_spec))
             sys.exit(0)
