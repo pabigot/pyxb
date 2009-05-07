2857c667201ecb58d40f22aeefb797d4033dda45
pab <devnull@localhost>
2009-05-07 19:42:57 +0000

    Fix specializations for soap12 namespace

--- pyxb/standard/bindings/soap12.py
+++ pyxb/standard/bindings/soap12.py
@@ -1,12 +1,11 @@
 from raw.soap12 import *
 import pyxb.standard.bindings.raw.soap12 as raw_soap12
-from pyxb.standard.bindings.wsdl import _WSDL_binding_mixin, _WSDL_tBinding_mixin
-
-class tBinding (raw_soap12.tBinding, _WSDL_tBinding_mixin):
-    pass
-raw_soap12.tBinding._SetClassRef(tBinding)
+from pyxb.standard.bindings.wsdl import _WSDL_binding_mixin, _WSDL_port_mixin
 
 class binding (raw_soap12.binding, _WSDL_binding_mixin):
     pass
 raw_soap12.binding._SetClassRef(binding)
     
+class address (raw_soap12.address, _WSDL_port_mixin):
+    pass
+raw_soap12.address._SetClassRef(address)
