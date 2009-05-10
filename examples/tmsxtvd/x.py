3e0f77856b3297454620056b4217efb37f61f3da
pab <devnull@localhost>
2009-05-10 02:15:20 +0000

    Extend example

--- examples/tmsxtvd/x.py
+++ examples/tmsxtvd/x.py
@@ -4,8 +4,15 @@ import xml.dom
 import xml.dom.minidom
 import pyxb.Namespace
 
+print 'Reading'
 xmls = open('tmsdatadirect_sample.xml').read()
+print 'Parsing'
 dom = xml.dom.minidom.parseString(xmls)
-
+print 'Generating binding'
 instance = tms.CreateFromDOM(dom.documentElement)
-print instance
+print 'Ready to go'
+for s in instance.stations().station():
+    print 'Station %s is %s, id %d' % (s.callSign(), s.name(), s.id())
+    
+for p in instance.programs().program():
+    print '%s "%s" episode %s: %s' % (p.showType(), p.title(),  p.syndicatedEpisodeNumber(), p.subtitle())
