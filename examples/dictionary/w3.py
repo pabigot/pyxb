f0048a61c413d3efe35630b5533b9ced18f6c8f8
pab <devnull@localhost>
2009-05-17 02:49:02 +0000

    Checkpoint before removal

--- examples/dictionary/w3.py
+++ examples/dictionary/w3.py
@@ -11,8 +11,8 @@ import pyxb.binding.generate
 
 import urllib2
 WSDL_uri = 'http://services.aonaware.com/DictService/DictService.asmx?WSDL'
-uri_src = urllib2.urlopen(WSDL_uri)
-uri_src = open('dictservice.wsdl')
+#uri_src = urllib2.urlopen(WSDL_uri)
+uri_src = open('dict.wsdl')
 doc = minidom.parseString(uri_src.read())
 
 #for ai in range(0, root.attributes.length):
@@ -26,13 +26,19 @@ for s in spec.service():
     if s.documentation():
         print s.documentation()
     for p in s.port():
+        b = p.bindingReference()
+        prot = b.protocolBinding()
+        if not (isinstance(prot, http.binding) and ('POST' == prot.verb())):
+            continue
         print '  Port %s at %s' % (p.name(), p.addressReference().location())
-        ptr = p.bindingReference().portTypeReference()
-        for op in p.bindingReference().operation():
+        ptr = b.portTypeReference()
+        for op in b.operation():
             pt_op = ptr.operationMap()[op.name()]
             if op.operationReference() is not None:
                 print '    %s (at %s)' % (op.name(), op.operationReference().locationInformation())
             else:
                 print '    %s' % (op.name(),)
             print '      %s' % (pt_op.documentation(),)
+            print '      Input: %s' % (pt_op.input()[0].message(),)
+            print '      Output: %s' % (pt_op.output()[0].message(),)
 
