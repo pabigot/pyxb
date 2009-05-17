ecc666f7dfc7ace45692c8759acff6d46bc1c43e
pab <devnull@localhost>
2009-05-17 21:16:00 +0000

    Change name to reference new schema name

--- tests/drivers/test-kml21.py
+++ tests/drivers/test-kml21.py
@@ -1,7 +1,7 @@
 import pyxb.binding.generate
 
 import os.path
-schema_path = '%s/../../pyxb/standard/schemas/kml21.xsd' % (os.path.dirname(__file__),)
+schema_path = '%s/../../pyxb/standard/schemas/kml.xsd' % (os.path.dirname(__file__),)
 code = pyxb.binding.generate.GeneratePython(schema_file=schema_path)
 rv = compile(code, 'test', 'exec')
 eval(rv)
