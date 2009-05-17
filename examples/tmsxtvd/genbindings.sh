e3a71ae9579b59d922c306e6407abafc41ef2280
pab <devnull@localhost>
2009-05-17 12:15:44 +0000

    Get and generate bindings

---
+++ examples/tmsxtvd/genbindings.sh
@@ -0,0 +1,19 @@
+PYTHONPATH=../..
+export PYTHONPATH
+URI='http://docs.tms.tribune.com/tech/xml/schemas/tmsxtvd.xsd'
+PREFIX='tmstvd'
+WSDL="${PREFIX}.wsdl"
+if [ ! -f ${WSDL} ] ; then
+  wget -O ${WSDL} "${URI}"
+fi
+
+mkdir -p raw
+touch raw/__init__.py
+../../scripts/genbind \
+   -m '' \
+   -p "${PREFIX}" \
+   -u "${WSDL}" \
+   -r 
+if [ ! -f ${PREFIX}.py ] ; then
+  echo "from raw.${PREFIX} import *" > ${PREFIX}.py
+fi
