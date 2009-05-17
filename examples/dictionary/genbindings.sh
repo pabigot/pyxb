592facaf17461d334f51bbbb1cc673faa36a2bf7
pab <devnull@localhost>
2009-05-17 12:43:26 +0000

    Document and add example

---
+++ examples/dictionary/genbindings.sh
@@ -0,0 +1,19 @@
+PYTHONPATH=../..
+export PYTHONPATH
+URI='http://services.aonaware.com/DictService/DictService.asmx?WSDL'
+PREFIX='dict'
+WSDL="${PREFIX}.wsdl"
+if [ ! -f ${WSDL} ] ; then
+  wget -O ${WSDL} "${URI}"
+fi
+
+mkdir -p raw
+touch raw/__init__.py
+../../scripts/genbind \
+   -m '' \
+   -p "${PREFIX}" \
+   -u "${WSDL}" \
+   -r -W
+if [ ! -f ${PREFIX}.py ] ; then
+  echo "from raw.${PREFIX} import *" > ${PREFIX}.py
+fi
---
+++ examples/dictionary/showdict.py
@@ -0,0 +1,23 @@
+import dict
+import urlparse
+import urllib2
+from xml.dom import minidom
+
+port_uri = 'http://services.aonaware.com/DictService/DictService.asmx'
+op_path = '/DictionaryList'
+uri = '%s%s' % (port_uri, op_path)
+dle_xml = urllib2.urlopen(uri).read()
+dle_dom = minidom.parseString(dle_xml)
+dle = dict.ArrayOfDictionary.CreateFromDOM(dle_dom.documentElement)
+
+op_path = '/DictionaryInfo'
+for d in dle.Dictionary():
+    print '%s (%s)' % (d.Name(), d.Id())
+    di_req = dict.DictionaryInfo(dictId=d.Id())
+    args = { }
+    for (t, eu) in di_req._ElementMap.items():
+        args[eu.tag()] = eu.value(di_req)
+    uri = '%s%s?%s' % (port_uri, op_path, '&'.join(['%s=%s' % _i for _i in args.items()]))
+    resp = urllib2.urlopen(uri).read()
+    di_resp = dict.CreateFromDOM(minidom.parseString(resp).documentElement)
+    print di_resp
