997320777559ed29991b1e2ad4817b04bf0b0aaf
pab <devnull@localhost>
2009-05-17 12:20:09 +0000

    Retrieve from URI, not wsdl

--- examples/tmsxtvd/genbindings.sh
+++ examples/tmsxtvd/genbindings.sh
@@ -2,21 +2,18 @@ PYTHONPATH=../..
 export PYTHONPATH
 URI='http://docs.tms.tribune.com/tech/xml/schemas/tmsxtvd.xsd'
 PREFIX='tmstvd'
-WSDL="${PREFIX}.wsdl"
-if [ ! -f ${WSDL} ] ; then
-  wget -O ${WSDL} "${URI}"
-fi
 
 mkdir -p raw
 touch raw/__init__.py
 ../../scripts/genbind \
    -m '' \
    -p "${PREFIX}" \
-   -u "${WSDL}" \
+   -u "${URI}" \
    -r 
 if [ ! -f ${PREFIX}.py ] ; then
   echo "from raw.${PREFIX} import *" > ${PREFIX}.py
 fi
+
 if [ ! -f tmsdatadirect_sample.xml ] ; then
   wget http://tmsdatadirect.com/docs/tv/tmsdatadirect_sample.xml
 fi
