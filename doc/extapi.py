39d1e34c20d59a59abbdeec9e5359729ccf34285
pab <devnull@localhost>
2009-05-19 13:42:57 +0000

    Stuff to support Sphinx builds

---
+++ doc/extapi.py
@@ -0,0 +1,87 @@
+# Kenozooid - software stack to support different capabilities of dive
+# computers.
+#
+# Copyright (C) 2009 by Artur Wroblewski &lt;wrobell@pld-linux.org&gt;
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+#
+
+import os.path
+from docutils import nodes
+import sys
+
+
+def api_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
+    """
+    Role `:api:` bridges generated API documentation by tool like EpyDoc
+    with Sphinx Python Documentation Generator.
+
+    Other tools, other than EpyDoc, can be easily supported as well.
+
+    First generate the documentation to be referenced, i.e. with EpyDoc::
+
+        $ mkdir -p doc/_build/html/api
+        $ epydoc -o doc/_build/html/api ...
+
+    Next step is to generate documentation with Sphinx::
+
+        $ sphinx-build doc doc/_build/html
+
+    """
+    basedir = 'api'
+    prefix = '_build/html/' # fixme: fetch it from configuration
+    exists = lambda f: os.path.exists(prefix + f)
+
+    # assume module is references
+    name = '%s' % text
+    uri = file = '%s/%s-module.html' % (basedir, text)
+    chunks = text.split('.')
+
+    print 'Trying module file %s' % (file,)
+
+    # if not module, then a class
+    if not exists(file):
+        name = text.split('.')[-1]
+        uri = file = '%s/%s-class.html' % (basedir, text)
+    print 'Trying class file %s' % (file,)
+
+    # if not a class, then function or class method 
+    if not exists(file):
+        method = chunks[-1]
+        fprefix = '.'.join(chunks[:-1])
+        # assume function is referenced
+        file = '%s/%s-module.html' % (basedir, fprefix)
+        print 'Trying method file %s' % (file,)
+        if exists(file):
+            uri = '%s#%s' % (file, method)
+        else:
+            # class method is references
+            file = '%s/%s-class.html' % (basedir, fprefix)
+            if exists(file):
+                name = '.'.join(chunks[-2:]) # name should be Class.method
+                uri = '%s/%s-class.html#%s' % (basedir, fprefix, method)
+
+    if exists(file):
+        node = nodes.reference(rawtext, name, refuri=uri, **options)
+    else:
+        # cannot find reference, then just inline the text
+        node = nodes.literal(rawtext, text)
+
+    return [node], []
+
+def setup(app):
+    app.add_role('api', api_role)
+    app.add_config_value('epydoc_basedir', 'api', False)
+    app.add_config_value('epydic_prefix', 'doc/_build/html/', False)
+    
