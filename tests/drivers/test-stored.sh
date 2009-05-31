3638d6536fba0e90fde359bcd05f9f642bd127bd
pab <devnull@localhost>
2009-05-31 15:10:37 +0000

    Update to new interfaces

--- tests/drivers/test-stored.sh
+++ tests/drivers/test-stored.sh
@@ -4,15 +4,15 @@ mkdir bindings
 PYXB_NAMESPACE_PATH='bindings:+'
 export PYXB_NAMESPACE_PATH
 
-../../scripts/pyxbgen --schema-uri ../schemas/shared-types.xsd --module-path bindings --schema-prefix st --save-component-model
-../../scripts/pyxbgen --schema-uri ../schemas/test-external.xsd --module-path bindings --schema-prefix te --save-component-model
+../../scripts/pyxbgen --schema-uri ../schemas/shared-types.xsd --module-path bindings.st --save-component-model
+../../scripts/pyxbgen --schema-uri ../schemas/test-external.xsd --module-path bindings.te --save-component-model
 OFN=test-stored-$$.py
 cat >>${OFN} <<EOText
-import pyxb.Namespace
-print "\n".join(pyxb.Namespace.AvailableForLoad())
-ns = pyxb.Namespace.NamespaceForURI('URN:shared-types', True)
+import pyxb
+print "\n".join(pyxb.namespace.AvailableForLoad())
+ns = pyxb.namespace.NamespaceForURI('URN:shared-types', True)
 ns.validateComponentModel()
-ns = pyxb.Namespace.NamespaceForURI('URN:test-external', True)
+ns = pyxb.namespace.NamespaceForURI('URN:test-external', True)
 ns.validateComponentModel()
 EOText
 python ${OFN}
