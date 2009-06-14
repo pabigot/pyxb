a4a432c1eea75f95fca2581bb4d5031ab6b05998
pab <devnull@localhost>
2009-06-14 17:16:20 +0000

    Checkpoint working (if noisy) version

--- pyxb/binding/saxer.py
+++ pyxb/binding/saxer.py
@@ -6,10 +6,67 @@ import xml.sax.handler
 import pyxb.namespace
 import ipo
 
+class _SAXElementState (object):
+    __parentState = None
+    namespaceContext = None
+    bindingObject = None
+    elementUse = None
+    enclosingCTD = None
+    __content = None
+
+    def __init__ (self, namespace_context, parent_state):
+        self.namespaceContext = namespace_context
+        self.__parentState = parent_state
+        self.bindingObject = None
+        self.elementUse = None
+        if isinstance(self.__parentState, _SAXElementState):
+            self.enclosingCTD = self.__parentState.enclosingCTD
+        else:
+            self.enclosingCTD = None
+        self.__content = []
+
+    def content (self):
+        return ''.join(self.__content)
+
+    def __constructElement (self, new_object_factory, attrs, content=None):
+        if content is not None:
+            self.bindingObject = new_object_factory(content)
+        else:
+            self.bindingObject = new_object_factory()
+        if isinstance(self.bindingObject, pyxb.binding.basis.complexTypeDefinition):
+            # @todo: set attributes
+            pass
+        if self.__elementUse is not None:
+            assert self.__parentState is not None
+            assert self.__parentState.bindingObject is not None
+            self.__elementUse.setOrAppend(self.__parentState.bindingObject, self.bindingObject)
+        return self.bindingObject
+
+    def startElement (self, type_class, new_object_factory, element_use, attrs):
+        self.__delayedConstructor = None
+        self.__elementUse = element_use
+        self.__attributes = None
+        if type_class._IsSimpleTypeContent():
+            self.__delayedConstructor = new_object_factory
+            self.__attributes = attrs
+        else:
+            self.__constructElement(new_object_factory, attrs)
+        return self.bindingObject
+
+    def addContent (self, content):
+        self.__content.append(content)
+
+    def endElement (self):
+        if self.__delayedConstructor is not None:
+            self.__constructElement(self.__delayedConstructor, self.__attributes, self.content())
+        return self.bindingObject
+
 class PyXBSAXHandler (xml.sax.handler.ContentHandler):
     __nextNamespaceContext = None
     __namespaceContext = None
     __XSITypeTuple = pyxb.namespace.XMLSchema_instance.createExpandedName('type').uriTuple()
+
+    __elementState = None
 
     def _updateNamespaceContext (self):
         if self.__nextNamespaceContext is not None:
@@ -18,8 +75,9 @@ class PyXBSAXHandler (xml.sax.handler.ContentHandler):
         return self.__namespaceContext
 
     def reset (self):
-        self.__nextNamespaceContext = None
         self.__namespaceContext = pyxb.namespace.NamespaceContext()
+        self.__elementState = _SAXElementState(self.__namespaceContext, None)
+        self.__nextNamespaceContext = None
 
     def __init__ (self):
         self.reset()
@@ -39,84 +97,83 @@ class PyXBSAXHandler (xml.sax.handler.ContentHandler):
     def endPrefixMapping (self, prefix):
         print 'End prefix %s' % (prefix,)
 
-    __rootObject = None
-    __contextStack = []
-
-    __bindingObject = None
-    __bindingStack = []
-    __enclosingCTD = None
-    __ctdStack = []
-    __elementUseStack = []
-
     def rootObject (self):
         return self.__rootObject
+    __rootObject = None
+
+    __elementStateStack = []
 
     def startElementNS (self, name, qname, attrs):
+
         name_en = pyxb.namespace.ExpandedName(name)
         ns_ctx = self._updateNamespaceContext()
-        self.__contextStack.append(ns_ctx)
+
+        parent_state = self.__elementState
+        self.__elementStateStack.append(self.__elementState)
+        self.__elementState = this_state = _SAXElementState(ns_ctx, parent_state)
 
         self.__nextNamespaceContext = pyxb.namespace.NamespaceContext(parent_context=ns_ctx)
 
-        print " ".join([ '%s=%s' % (_v, _ns.uri()) for (_v, _ns) in self.__namespaceContext.inScopeNamespaces().items() ])
         print 'Start element %s with %d attrs' % (pyxb.namespace.ExpandedName(name), len(attrs))
         for attr_name in attrs.getNames():
             attr_en = pyxb.namespace.ExpandedName(attr_name)
             attr_val = attrs.getValue(attr_name)
             print '  %s %s' % (attr_en, attr_val)
+
         type_class = element_use = element_binding = None
+
         if attrs.has_key(self.__XSITypeTuple):
             xsi_type = attrs.getValue(self.__XSITypeTuple)
             type_class = ns_ctx.interpretQName(xsi_type).typeBinding()
-        if self.__enclosingCTD is not None:
-            element_use = self.__enclosingCTD._UseForTag(name_en)
+
+        # @todo: handle substitution groups
+        print 'Parent enclosing CTD %s' % (parent_state.enclosingCTD,)
+        if parent_state.enclosingCTD is not None:
+            element_use = parent_state.enclosingCTD._UseForTag(name_en)
         else:
             element_binding = name_en.elementBinding()
+
         if element_use is not None:
             assert self.__rootObject is not None
             element_binding = element_use.elementBinding()
+            assert element_binding is not None
 
-        self.__bindingStack.append(self.__bindingObject)
-        self.__elementUseStack.append(element_use)
         if type_class is not None:
-            new_object = type_class.Factory(_validate_constraints=False)
+            # @todo: validate xsi:type against abstract
+            new_object_factory = type_class.Factory
         else:
             assert element_binding is not None
-            new_object = element_binding(_validate_constraints=False)
+            new_object_factory = element_binding
+            type_class = element_binding.typeDefinition()
 
-        if element_use is None:
-            assert self.__rootObject is None
-            self.__rootObject = new_object
+        assert type_class is not None
+        if issubclass(type_class, pyxb.binding.basis.complexTypeDefinition):
+            this_state.enclosingCTD = type_class
         else:
-            element_use.setOrAppend(self.__bindingObject, new_object)
-        self.__bindingObject = new_object
+            this_state.enclosingCTD = parent_state.enclosingCTD
 
-        if isinstance(self.__bindingObject, pyxb.binding.basis.complexTypeDefinition):
-            self.__ctdStack.append(self.__enclosingCTD)
-            self.__enclosingCTD = self.__bindingObject
+        binding_object = this_state.startElement(type_class, new_object_factory, element_use, attrs)
+        if self.__rootObject is None:
+            self.__rootObject = binding_object
 
     def endElementNS (self, name, qname):
-        element_use = self.__elementUseStack.pop()
-        if isinstance(self.__bindingObject, pyxb.binding.basis.simpleTypeDefinition):
-            # Note: Not doing this for CTDs, takes too long
-            self.__bindingObject.validateBinding()
+        this_state = self.__elementState
+        parent_state = self.__elementState = self.__elementStateStack.pop()
+        binding_object = this_state.endElement()
+        assert binding_object is not None
 
-        if self.__enclosingCTD == self.__bindingObject:
-            self.__enclosingCTD = self.__ctdStack.pop()
-        self.__bindingObject = self.__bindingStack.pop()
-        self.__nextNamespaceContext = None
-        self.__namespaceContext = self.__contextStack.pop()
+        if self.__rootObject is None:
+            self.__rootObject = binding_object
+
         print 'End element %s' % (pyxb.namespace.ExpandedName(name),)
 
     def characters (self, content):
         print 'Content %s' % (content,)
-        if isinstance(self.__bindingObject, pyxb.binding.basis.simpleTypeDefinition):
-            pass
-        else:
-            self.__bindingObject.append(content)
+        self.__elementState.addContent(content)
 
     def ignorableWhitespace (self, whitespace):
         print 'Whitespace %s' % (whitespace,)
+        self.__elementState.addContent(whitespace)
         pass
 
 def ShowOrder (order):
