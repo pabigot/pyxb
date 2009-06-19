4307c4ca95637851653a045aef928abc8c86b5cd
pab <devnull@localhost>
2009-06-19 16:48:51 +0000

    Fill out gYear testing

--- tests/datatypes/test-gYear.py
+++ tests/datatypes/test-gYear.py
@@ -1,9 +1,24 @@
+import pyxb
 import unittest
 import pyxb.binding.datatypes as xsd
 
 class Test_gYear (unittest.TestCase):
-    def testRange (self):
-        self.assertFalse("Datatype gYear test not implemented")
+    def testBasic (self):
+        v = xsd.gYear('1234')
+        self.assertEqual(v.year, 1234)
+        v = xsd.gYear(1234)
+        self.assertEqual(v.year, 1234)
 
+    def testXSDLiteral (self):
+        v = xsd.gYear(1234)
+        self.assertEqual('1234', v.xsdLiteral())
+
+    def testAccessor (self):
+        v = xsd.gYear(1234)
+        self.assertRaises(AttributeError, getattr, v, 'month')
+        self.assertRaises(AttributeError, setattr, v, 'month', 5)
+        self.assertRaises(AttributeError, setattr, v, 'year', 5)
+
+        
 if __name__ == '__main__':
     unittest.main()
