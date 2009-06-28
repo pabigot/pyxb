a99ceec9c06e905be2a59b181c5304b69e48056e
pab <devnull@localhost>
2009-06-28 02:01:15 +0000

    Ensure namespace archive is re-written

--- examples/manual/demo.sh
+++ examples/manual/demo.sh
@@ -2,14 +2,19 @@ PYTHONPATH=../..:.
 export PYTHONPATH
 PYXB_NAMESPACE_PATH=.:+
 export PYXB_NAMESPACE_PATH
-rm po1.py
+rm -f po1.py
 ../../scripts/pyxbgen -u po1.xsd  -m po1
 python demo1.py
-rm po2.py
+rm -f po2.py
 ../../scripts/pyxbgen -u po2.xsd  -m po2
 python demo2.py
+rm -f address.py address.wxs
 ../../scripts/pyxbgen -u nsaddress.xsd -m address -C
-rm po3.py
+if [ ! -f address.wxs ] ; then
+  echo 1>&2 "Address namespace archive not found"
+  exit 1
+fi
+rm -f po3.py
 ../../scripts/pyxbgen -u po3.xsd -m po3
 python demo3.py
 #rm po4.py
