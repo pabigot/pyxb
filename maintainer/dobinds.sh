252ceba2f123f4e8b0cf348474e7225bd96fadc6
pab <devnull@localhost>
2009-07-01 12:53:18 +0000

    Improve description of why we do not do the XMLSchema schema

--- maintainer/dobinds.sh
+++ maintainer/dobinds.sh
@@ -12,9 +12,11 @@ test -f ${SCHEMA_DIR}/XMLSchema.xsd || wget -O ${SCHEMA_DIR}/XMLSchema.xsd http:
 # NOTE: Use prefix xml_ instead of xml because xml conflicts with standard package
 # This isn't really necessary, and currently fails resolution, so drop it
 #http://www.w3.org/2001/xml.xsd xml_
-# NOTE: XSD depends on XML which depends on XSD.  Since we have a workable
-# XSD schema built-in, do not create an external pre-loaded schema for
-# which we'll have to resolve in XML and subsequently in XSD.
+
+# NOTE: The non-normative XMLSchema namespace defines far more than
+# the public components for which we have built-in values.  The
+# incompleteness of the built-ins confuses PyXB.  There's no need to
+# fix this since we have a workable XSD schema built-in.
 #http://www.w3.org/2001/XMLSchema.xsd xsd
 http://www.w3.org/2001/XMLSchema-hasFacetAndProperty xsd_hfp
 http://schemas.xmlsoap.org/wsdl/ wsdl
