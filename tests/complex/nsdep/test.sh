2431bc6683a6fbc770fa2cb80f34a527170f0734
pab <devnull@localhost>
2009-07-04 20:49:04 +0000

    Make more complex

--- tests/complex/nsdep/test.sh
+++ tests/complex/nsdep/test.sh
@@ -3,6 +3,7 @@ PYTHONPATH=../../..
 rm -rf bindings
 mkdir -p bindings
 touch bindings/__init__.py
-../../../scripts/pyxbgen -m bindings.A -u a_c.xsd -C
-../../../scripts/pyxbgen -m bindings.B -u b_c.xsd -C
+#../../../scripts/pyxbgen -m bindings.A -u a_c.xsd -C
+#../../../scripts/pyxbgen -m bindings.B -u b_c.xsd -C
+../../../scripts/pyxbgen -m bindings.D -u d_c.xsd
 
