7ba2493f95e17ac317d8dfebac1d0f660eebf77b
pab <devnull@localhost>
2009-07-07 13:10:11 +0000

    Update to new interface

--- tests/complex/nsdep/test.sh
+++ tests/complex/nsdep/test.sh
@@ -3,7 +3,11 @@ PYTHONPATH=../../..
 rm -rf bindings
 mkdir -p bindings
 touch bindings/__init__.py
+
 #../../../scripts/pyxbgen -m bindings.A -u a_c.xsd -C
 #../../../scripts/pyxbgen -m bindings.B -u b_c.xsd -C
-../../../scripts/pyxbgen -m bindings.D -u d_c.xsd
+../../../scripts/pyxbgen \
+  --module-prefix=bindings \
+  --schema-location=d_c.xsd --module=D \
+  --archive-file=bindings/D.wxs
 
