e17f0c033a47f447939e42b11d4914a77b8a70d9
pab <devnull@localhost>
2009-07-16 19:01:32 +0000

    Clean up example, add to test

--- examples/OpenGIS/test.sh
+++ examples/OpenGIS/test.sh
@@ -1,11 +1,18 @@
 export PYXB_ARCHIVE_PATH=opengis:opengis/misc:opengis/citygml:opengis/iso19139:+
 PYXB_ROOT=../..
 export PYTHONPATH=${PYXB_ROOT}:.
+export PATH=${PYXB_ROOT}/scripts:${PATH}
 
 if [ ! -d opengis ] ; then
   sh makebind.sh
 fi
 
 python demo.py || exit 1
-python check_sos.py Schemas/sos/1.0.0/examples/*.xml
+#python check_sos.py Schemas/sos/1.0.0/examples/*.xml
 
+rm gmlapp.py raw/gmlapp.py
+pyxbgen \
+  --schema-location=gmlapp.xsd --module=gmlapp \
+  --write-for-customization
+python testgml.py
+  
--- examples/OpenGIS/testgml.py
+++ examples/OpenGIS/testgml.py
@@ -2,54 +2,32 @@
 
 import opengis.gml_3_2 as gml
 import gmlapp
+import pyxb.utils.domutils
 
-env = gml.GridEnvelopeType([0, 0], [4, 4])
-#print env.toxml()
-limits = gml.GridLimitsType(env)
-point = gml.pos([-93.25, 43.5])
+limits = gml.GridLimitsType(gml.GridEnvelopeType([0, 0], [4, 4]))
 origin = gml.PointPropertyType(gml.Point(gml.pos([-93.25, 43.5]), id='_origin'))
 offset_vector = gml.VectorType([0, 0])
-
 grid = gml.RectifiedGrid(limits, 'latitude longitude', origin, offset_vector, dimension=2)
 grid.setId('_%x' % id(grid))
 domain = gml.domainSet(grid)
-#print domain.toxml()
 
 val_template = gmlapp.Temperature(nilReason='template', _nil=True, uom='urn:x-si:v1999:uom:degreesC')
-val_instance = gmlapp.Temperature(34.2, uom='urn:x-si:v1999:uom:degreesC')
-#instance = gml.valueComponent()
-#instance.setAbstractValue(val_instance)
-instance = gml.valueComponent(AbstractValue=val_instance)
-#print instance.toxml()
-
-xml = '''<?xml version="1.0" ?>
-<gml:valueComponent xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:app="URN:gmlapp"><app:Temperature uom="urn:x-si:v1999:uom:degreesC">34.2</app:Temperature></gml:valueComponent>
-'''
-instance = gml.CreateFromDocument(xml)
-
-instance = gml.valueComponents(AbstractValue=[val_instance])
-#print instance.toxml()
-
-xml = '''<?xml version="1.0" ?>
-<gml:valueComponents xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:app="URN:gmlapp"><app:Temperature uom="urn:x-si:v1999:uom:degreesC">34.2</app:Temperature></gml:valueComponents>
-'''
-instance = gml.CreateFromDocument(xml)
-
 vc = gml.valueComponents(AbstractValue=val_template)
 rp = gml.rangeParameters(gml.CompositeValue(vc, id='_vc'))
 data = gml.tupleList('34.2 35.4')
 range = gml.rangeSet(DataBlock=gml.DataBlockType(rp, data))
 
-gc = gml.RectifiedGridCoverage(domain, range)
-gc.setId('_%x' % (id(gc),))
+rgc = gml.RectifiedGridCoverage(domain, range)
+rgc.setId('_%x' % (id(rgc),))
 
-xml = gc.toxml()
-print xml
+bds = pyxb.utils.domutils.BindingDOMSupport(namespace_prefix_map={ gml.Namespace : 'gml' , gmlapp.Namespace : 'app' })
+
+xml = rgc.toxml(bds)
 instance = gml.CreateFromDocument(xml)
-xml2 = instance.toxml()
+bds.reset()
+xml2 = instance.toxml(bds)
 
 assert xml == xml2
-
 print xml2
 
 
