a05a2ed34036be6749a105471db96c2537ff06f3
pab <devnull@localhost>
2009-08-04 03:06:28 +0000

    Add application that depends on extended common

--- tests/complex/nsext/test.sh
+++ tests/complex/nsext/test.sh
@@ -1,18 +1,37 @@
 PYXB_ROOT=../../..
 PYTHONPATH=${PYXB_ROOT}:.
 PATH=${PYXB_ROOT}/scripts:${PATH}
-PYXB_ARCHIVE_PATH=.:+
+PYXB_ARCHIVE_PATH=.
 export PYTHONPATH PATH PYXB_ARCHIVE_PATH
 
-rm -f *.wxs common.py common4app.py
+# cat >/dev/null <<EOT
+
+rm -f *.wxs *.pyc common.py common4app.py app.py
+
+exit 0
 
 pyxbgen \
   --schema-location=base.xsd --module=common \
   --archive-to-file=common.wxs
 
+pyxbdump common.wxs
+
+echo '**************************'
+
 pyxbgen \
   --schema-location=extend.xsd --module=common4app \
+  --archive-to-file=common4app.wxs
   
+pyxbdump common4app.wxs
+
+# EOT
+
 python tst-base.py \
 && python tst-extend.py \
-&& echo 'Passed'
+&& echo 'Passed common tests'
+
+pyxbgen \
+  --schema-location=app.xsd --module=app
+  
+python tst-app.py \
+&& echo 'Passed application tests'
---
+++ tests/complex/nsext/tst-app.py
@@ -0,0 +1,6 @@
+import app
+import common4app
+
+x = common4app.extended('hi', 'there')
+a = app.elt(x)
+print a.toxml()
