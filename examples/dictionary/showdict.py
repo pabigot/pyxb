f5d347f5f7597f7d0ad499ab67a7c268b2d16370
pab <devnull@localhost>
2009-08-08 23:35:03 +0000

    Eliminate irritating failure

--- examples/dictionary/showdict.py
+++ examples/dictionary/showdict.py
@@ -18,4 +18,6 @@ for d in dle.Dictionary:
     # The response is a simple type derived from string, so we can
     # just extract and print it.
     di_resp = dict.CreateFromDOM(domutils.StringToDOM(resp))
-    print "%s (%s)\n%s\n" % (d.Name, d.Id, di_resp)
+    # Do the "encode" garbage because one of these dictionaries has a
+    # non-ASCII character
+    print "%s (%s)\n%s\n" % (d.Name.encode('utf-8'), d.Id.encode('utf-8'), di_resp.encode('utf-8'))
