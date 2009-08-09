b12a4cfb58eec8356298218846d27e18a5407f5b
pab <devnull@localhost>
2009-08-09 17:56:47 +0000

    Start integrating opengis

--- maintainer/bundlesupport.sh
+++ maintainer/bundlesupport.sh
@@ -9,8 +9,7 @@ PYXB_ROOT=${PYXB_ROOT:-/home/pab/pyxb/dev}
 MODULE_PREFIX=pyxb.bundles.${BUNDLE_TAG}
 BUNDLE_ROOT=${PYXB_ROOT}/pyxb/bundles/${BUNDLE_TAG}
 SCHEMA_DIR=${BUNDLE_ROOT}/schemas
-BINDINGS_DIR=${BUNDLE_ROOT}/bindings
-RAW_DIR=${BINDINGS_DIR}/raw
+RAW_DIR=${BUNDLE_ROOT}/raw
 rm -rf ${RAW_DIR}
 mkdir -p ${RAW_DIR}
 touch ${RAW_DIR}/__init__.py
