8bad4f138ce8e361b1d29841b41cb6ead7f1ffed
pab <devnull@localhost>
2009-08-10 00:46:08 +0000

    Use recursive under bundles as archive path

--- pyxb/namespace/archive.py
+++ pyxb/namespace/archive.py
@@ -30,7 +30,7 @@ C{pyxb/standard/bindings/raw})."""
 import os.path
 import stat
 
-DefaultArchivePath = os.path.realpath("%s/bundles/core/raw" % (os.path.join(os.path.dirname( __file__), '..'),))
+DefaultArchivePath = '%s//' % (os.path.realpath('%s/bundles' % (os.path.join(os.path.dirname( __file__), '..'),)),)
 """Default location for reading C{.wxs} files"""
 
 def GetArchivePath ():
