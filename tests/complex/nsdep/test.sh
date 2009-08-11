96d9cd890c90f5ab9a978d155a8c7e5e9d2fc941
pab <devnull@localhost>
2009-08-11 00:16:37 +0000

    Start move towards independent autotest infrastructure

--- tests/complex/nsdep/test.sh
+++ tests/complex/nsdep/test.sh
@@ -1,12 +1,10 @@
-PYXB_ARCHIVE_PATH=bindings:+
-PYTHONPATH=../../..
+PYXB_ARCHIVE_PATH=${PYXB_ARCHIVE_PATH:+${PYXB_ARCHIVE_PATH}:}bindings
+# PYTHONPATH=${PYTHONPATH:+${PYTHONPATH}:}bindings:.
 rm -rf bindings
 mkdir -p bindings
 touch bindings/__init__.py
 
-#../../../scripts/pyxbgen -m bindings.A -u a_c.xsd -C
-#../../../scripts/pyxbgen -m bindings.B -u b_c.xsd -C
-../../../scripts/pyxbgen \
+pyxbgen \
   --module-prefix=bindings \
   --schema-location=d_c.xsd --module=D \
   --archive-to-file=bindings/D.wxs \
--- tests/complex/nsdep/tst-a.py
+++ tests/complex/nsdep/tst-a.py
@@ -1,4 +1,18 @@
-import bindings.A as A
+from pyxb.utils.domutils import BindingDOMSupport
+import unittest
 
-x = A.A_c_e1("A_b_e1", "e1")
-print x.toxml()
+import bindings._A as A
+
+class Test (unittest.TestCase):
+    def setUp (self):
+        BindingDOMSupport.DeclareNamespace(A.Namespace, 'a')
+        
+    def tearDown (self):
+        BindingDOMSupport.Reset(prefix_map=True)
+        
+    def tests (self):
+        x = A.A_c_e1("A_b_e1", "e1")
+        self.assertEqual('<a:A_c_e1 xmlns:a="URN:nsdep:A"><a:A_b_e1>A_b_e1</a:A_b_e1><a:e1>e1</a:e1></a:A_c_e1>', x.toxml(root_only=True))
+
+if '__main__' == __name__:
+    unittest.main()
