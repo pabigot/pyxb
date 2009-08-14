ab831080fb3033f04a2e235c81de9ae5d7d0c0f2
pab <devnull@localhost>
2009-08-14 00:42:40 +0000

    New role for references to Python exceptions.  Created under misapprehension (wanted this for the API documentation, not Sphinx)

--- doc/extapi.py
+++ doc/extapi.py
@@ -116,10 +116,31 @@ def ticket_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
     return [node], []
 
 
+def pyex_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
+    """
+    Role `:pyex:` generates reference to Python exception classes.
+    """
+
+    pyex_fmt = 'http://docs.python.org/library/exceptions.html#exceptions.%s'
+    mo = __Reference_re.match(text)
+    label = None
+    if mo is not None:
+        ( label, text ) = mo.group(1, 2)
+    exc = text.strip()
+    print 'Python exception %s as %s' % (text, label)
+
+    uri = pyex_fmt % (exc,)
+    if label is None:
+        label = '%s' % (exc,)
+    node = nodes.reference(rawtext, label, refuri=uri, **options)
+
+    return [node], []
+
 def setup(app):
     app.add_role('api', api_role)
     app.add_config_value('epydoc_basedir', 'api', False)
     app.add_config_value('epydoc_prefix', 'doc/html/', False)
     app.add_role('ticket', ticket_role)
     app.add_config_value('epydoc_track_root', 'http://sourceforge.net/apps/track/project', False)
+    app.add_role('pyex', pyex_role)
     
