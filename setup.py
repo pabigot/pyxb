c01592040651fff3fdd65a031c921c4a4d415fa6
pab <devnull@localhost>
2009-08-25 12:33:03 +0000

    Update for next development

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ setup(name='PyXB',
       author_email='pyxb@comcast.net',
       url='http://pyxb.sourceforge.net',
       # Also change in README.TXT, pyxb/__init__.py, and doc/conf.py
-      version='0.7.2',
+      version='0.7.3-DEV',
       provides='pyxb',
       packages=[
         'pyxb', 'pyxb.namespace', 'pyxb.binding', 'pyxb.utils', 'pyxb.xmlschema',
