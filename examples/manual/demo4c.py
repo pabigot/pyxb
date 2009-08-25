9d279077a72be29b7dad09febd9fc3d0e3edd5ad
pab <devnull@localhost>
2009-08-25 02:57:01 +0000

    Fix date to avoid unnecessary changes

--- examples/manual/demo4c.py
+++ examples/manual/demo4c.py
@@ -5,7 +5,7 @@ import po4
 import address
 import pyxb.binding.datatypes as xs
 
-po = po4.purchaseOrder(orderDate=xs.date.today())
+po = po4.purchaseOrder(orderDate=xs.date(1999, 10, 20))
 po.shipTo = address.USAddress('Alice Smith', '123 Maple Street', 'Anytown', 'AK', 12341)
 po.billTo = address.USAddress('Robert Smith', '8 Oak Avenue', 'Anytown', 'AK', 12341)
                 
