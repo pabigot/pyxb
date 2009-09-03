ddd9f553d11518f70aac00f36a0cfbb411b2ae76
pab@services.mn.pab <pab@services.mn.pab>
2009-09-03 18:54:42 -0500

    Complete characterization of code point sets

--- pyxb/utils/unicode.py
+++ pyxb/utils/unicode.py
@@ -75,6 +75,7 @@ class CodePointSet (object):
             raise ValueError('codepoint range value order')
         if s > self.MaxCodePoint:
             return self
+        e = min(e, self.MaxCodePoint)
         li = bisect.bisect_left(self.__codepoints, s)
         ri = bisect.bisect_right(self.__codepoints, e)
         case = ((li & 1) << 1) | (ri & 1)
@@ -94,18 +95,40 @@ class CodePointSet (object):
         return self
 
     def add (self, value):
+        """Add the given value to the code point set.
+
+        @param value: An integral value denoting a code point, or a
+        tuple C{(s,e)} denoting the start and end (inclusive) code
+        points in a range.
+        @return: C{self}"""
         return self.__mutate(value, True)
 
     def extend (self, values):
-        [ self.__mutate(_v, True) for _v in values ]
+        """Add multiple values to a code point set.
+
+        @param values: Either a L{CodePointSet} instance, or an iterable
+        whose members are valid parameters to L{add}.
+
+        @return: C{self}"""
+        if isinstance(values, CodePointSet):
+            self.extend(values.asTuples())
+        else:
+            [ self.__mutate(_v, True) for _v in values ]
         return self
 
     def subtract (self, value):
+        """Remove the given value from the code point set.
+
+        @param value: An integral value denoting a code point, or a tuple
+        C{(s,e)} denoting the start and end (inclusive) code points in a
+        range.
+
+        @return: C{self}"""
         return self.__mutate(value, False)
 
     def asTuples (self):
-        """Return the codepoints as tuples denoting the ranges that
-        are in the set.
+        """Return the codepoints as tuples denoting the ranges that are in
+        the set.
 
         Each tuple C{(s, e)} indicates that the code points from C{s}
         (inclusive) to C{e}) (inclusive) are in the set."""
@@ -123,8 +146,7 @@ class CodePointSet (object):
         return rv
 
     def negate (self):
-        """Return an instance that represents the inverse of this
-        set."""
+        """Return an instance that represents the inverse of this set."""
         rv = type(self)()
         if (0 < len(self.__codepoints)) and (0 == self.__codepoints[0]):
             rv.__codepoints.extend(self.__codepoints[1:])
@@ -136,60 +158,52 @@ class CodePointSet (object):
 
 from unicode_data import *
 
-# Classes from Appendix B of XML 2e
-_Letter = CodePointSet( # BaseChar
-(0x0041, 0x005A), (0x0061, 0x007A), (0x00C0, 0x00D6), (0x00D8, 0x00F6),
-(0x00F8, 0x00FF), (0x0100, 0x0131), (0x0134, 0x013E), (0x0141, 0x0148),
-(0x014A, 0x017E), (0x0180, 0x01C3), (0x01CD, 0x01F0), (0x01F4, 0x01F5),
-(0x01FA, 0x0217), (0x0250, 0x02A8), (0x02BB, 0x02C1), 0x0386, (0x0388, 0x038A),
-0x038C, (0x038E, 0x03A1), (0x03A3, 0x03CE), (0x03D0, 0x03D6), 0x03DA, 0x03DC,
-0x03DE, 0x03E0, (0x03E2, 0x03F3), (0x0401, 0x040C), (0x040E, 0x044F),
-(0x0451, 0x045C), (0x045E, 0x0481), (0x0490, 0x04C4), (0x04C7, 0x04C8),
-(0x04CB, 0x04CC), (0x04D0, 0x04EB), (0x04EE, 0x04F5), (0x04F8, 0x04F9),
-(0x0531, 0x0556), 0x0559, (0x0561, 0x0586), (0x05D0, 0x05EA), (0x05F0, 0x05F2),
-(0x0621, 0x063A), (0x0641, 0x064A), (0x0671, 0x06B7), (0x06BA, 0x06BE),
-(0x06C0, 0x06CE), (0x06D0, 0x06D3), 0x06D5, (0x06E5, 0x06E6), (0x0905, 0x0939),
-0x093D, (0x0958, 0x0961), (0x0985, 0x098C), (0x098F, 0x0990), (0x0993, 0x09A8),
-(0x09AA, 0x09B0), 0x09B2, (0x09B6, 0x09B9), (0x09DC, 0x09DD), (0x09DF, 0x09E1),
-(0x09F0, 0x09F1), (0x0A05, 0x0A0A), (0x0A0F, 0x0A10), (0x0A13, 0x0A28),
-(0x0A2A, 0x0A30), (0x0A32, 0x0A33), (0x0A35, 0x0A36), (0x0A38, 0x0A39),
-(0x0A59, 0x0A5C), 0x0A5E, (0x0A72, 0x0A74), (0x0A85, 0x0A8B), 0x0A8D,
-(0x0A8F, 0x0A91), (0x0A93, 0x0AA8), (0x0AAA, 0x0AB0), (0x0AB2, 0x0AB3),
-(0x0AB5, 0x0AB9), 0x0ABD, 0x0AE0, (0x0B05, 0x0B0C), (0x0B0F, 0x0B10),
-(0x0B13, 0x0B28), (0x0B2A, 0x0B30), (0x0B32, 0x0B33), (0x0B36, 0x0B39), 0x0B3D,
-(0x0B5C, 0x0B5D), (0x0B5F, 0x0B61), (0x0B85, 0x0B8A), (0x0B8E, 0x0B90),
-(0x0B92, 0x0B95), (0x0B99, 0x0B9A), 0x0B9C, (0x0B9E, 0x0B9F), (0x0BA3, 0x0BA4),
-(0x0BA8, 0x0BAA), (0x0BAE, 0x0BB5), (0x0BB7, 0x0BB9), (0x0C05, 0x0C0C),
-(0x0C0E, 0x0C10), (0x0C12, 0x0C28), (0x0C2A, 0x0C33), (0x0C35, 0x0C39),
-(0x0C60, 0x0C61), (0x0C85, 0x0C8C), (0x0C8E, 0x0C90), (0x0C92, 0x0CA8),
-(0x0CAA, 0x0CB3), (0x0CB5, 0x0CB9), 0x0CDE, (0x0CE0, 0x0CE1), (0x0D05, 0x0D0C),
-(0x0D0E, 0x0D10), (0x0D12, 0x0D28), (0x0D2A, 0x0D39), (0x0D60, 0x0D61),
-(0x0E01, 0x0E2E), 0x0E30, (0x0E32, 0x0E33), (0x0E40, 0x0E45), (0x0E81, 0x0E82),
-0x0E84, (0x0E87, 0x0E88), 0x0E8A, 0x0E8D, (0x0E94, 0x0E97), (0x0E99, 0x0E9F),
-(0x0EA1, 0x0EA3), 0x0EA5, 0x0EA7, (0x0EAA, 0x0EAB), (0x0EAD, 0x0EAE), 0x0EB0,
-(0x0EB2, 0x0EB3), 0x0EBD, (0x0EC0, 0x0EC4), (0x0F40, 0x0F47), (0x0F49, 0x0F69),
-(0x10A0, 0x10C5), (0x10D0, 0x10F6), 0x1100, (0x1102, 0x1103), (0x1105, 0x1107),
-0x1109, (0x110B, 0x110C), (0x110E, 0x1112), 0x113C, 0x113E, 0x1140, 0x114C,
-0x114E, 0x1150, (0x1154, 0x1155), 0x1159, (0x115F, 0x1161), 0x1163, 0x1165,
-0x1167, 0x1169, (0x116D, 0x116E), (0x1172, 0x1173), 0x1175, 0x119E, 0x11A8,
-0x11AB, (0x11AE, 0x11AF), (0x11B7, 0x11B8), 0x11BA, (0x11BC, 0x11C2), 0x11EB,
-0x11F0, 0x11F9, (0x1E00, 0x1E9B), (0x1EA0, 0x1EF9), (0x1F00, 0x1F15),
-(0x1F18, 0x1F1D), (0x1F20, 0x1F45), (0x1F48, 0x1F4D), (0x1F50, 0x1F57), 0x1F59,
-0x1F5B, 0x1F5D, (0x1F5F, 0x1F7D), (0x1F80, 0x1FB4), (0x1FB6, 0x1FBC), 0x1FBE,
-(0x1FC2, 0x1FC4), (0x1FC6, 0x1FCC), (0x1FD0, 0x1FD3), (0x1FD6, 0x1FDB),
-(0x1FE0, 0x1FEC), (0x1FF2, 0x1FF4), (0x1FF6, 0x1FFC), 0x2126, (0x212A, 0x212B),
-0x212E, (0x2180, 0x2182), (0x3041, 0x3094), (0x30A1, 0x30FA), (0x3105, 0x312C),
-(0xAC00, 0xD7A3)
-    # Ideographic
-    ).extend([ (0x4E00, 0x9FA5), 0x3007, (0x3021, 0x3029) ])
-_Extender = CodePointSet(0x00B7, 0x02D0, 0x02D1, 0x0387, 0x0640, 0x0E46, 0x0EC6,
-                         0x3005, (0x3031, 0x3035), (0x309D, 0x309E), (0x30FC, 0x30FE) )
+# Some of the MultiCharEsc classes refer to the U{NameChar
+# <http://www.w3.org/TR/REC-xml/#NT-NameChar>} production for base
+# XML.  The XMLSchema 1.0 definition refers to the 2nd edition of XML,
+# which defines in Annex B the set of relevant character classes based
+# on Unicode 2.0.  However, the current (fifth, at this writing)
+# edition uses a much simpler characterization, and I'm going with
+# that one.
+_NameStartChar = CodePointSet(ord(':'),
+                              ( ord('A'), ord('Z') ),
+                              ( ord('a'), ord('z') ),
+                              (    0xC0,    0xD6 ),
+                              (    0xD8,    0xF6 ),
+                              (    0xF8,   0x2FF ),
+                              (   0x370,   0x37D ),
+                              (   0x37F,  0x1FFF ),
+                              (  0x200C,  0x200D ),
+                              (  0x2070,  0x218F ),
+                              (  0x2C00,  0x2FEF ),
+                              (  0x3001,  0xD7FF ),
+                              (  0xF900,  0xFDCF ),
+                              (  0xFDF0,  0xFFFD ),
+                              ( 0x10000, 0xEFFFF ) )
+_NameChar = CodePointSet(_NameStartChar).extend([ ord('-'),
+                                                  ord('.'),
+                                                  ( ord('0'), ord('9') ),
+                                                  0xB7,
+                                                  ( 0x0300, 0x036F ),
+                                                  ( 0x203F, 0x2040 ) ])
 
+# Production 24 : Single Character Escapes
+SingleCharEsc = { 'n' : CodePointSet(0x0A),
+                  'r' : CodePointSet(0x0D),
+                  't' : CodePointSet(0x09) }
+for c in r'\|.-^?*+{}()[]':
+    SingleCharEsc[c] = CodePointSet(ord(c))
 
+# Production 37 : Multi-Character Escapes
 WildcardEsc = CodePointSet(ord('\n'), ord('\r')).negate()
-MultiCharEsc = {}
+MultiCharEsc = { }
 MultiCharEsc['s'] = CodePointSet(0x20, ord('\t'), ord('\n'), ord('\r'))
 MultiCharEsc['S'] = MultiCharEsc['s'].negate()
-MultiCharEsc['i'] = CodePointSet(PropertyMap['L']).add(ord('_')).add(ord(':'))
+MultiCharEsc['i'] = _NameStartChar
 MultiCharEsc['I'] = MultiCharEsc['i'].negate()
-
+MultiCharEsc['c'] = _NameChar
+MultiCharEsc['C'] = MultiCharEsc['c'].negate()
+MultiCharEsc['d'] = PropertyMap['Nd']
+MultiCharEsc['D'] = MultiCharEsc['d'].negate()
+MultiCharEsc['W'] = CodePointSet(PropertyMap['P']).extend(PropertyMap['Z']).extend(PropertyMap['C'])
+MultiCharEsc['w'] = MultiCharEsc['W'].negate()
