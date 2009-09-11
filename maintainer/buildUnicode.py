7c247fc6287dce23549aee09dad48e76dc958e82
pab <bigot@acm.org>
2009-09-11 08:02:20 -0700

    Add copyright, document purpose

--- maintainer/buildUnicode.py
+++ maintainer/buildUnicode.py
@@ -1,9 +1,31 @@
+# Copyright 2009, Peter A. Bigot
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain a
+# copy of the License at:
+#
+#            http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Utility to generate the code point sets defined by the Unicode standard.
+
+You'll need these files:
+
+ - U{http://www.unicode.org/Public/3.1-Update/UnicodeData-3.1.0.txt}
+ - U{http://www.unicode.org/Public/3.1-Update/Blocks-4.txt}
+
+Invoke this script, redirecting the output to
+C{pyxb/utils/unicode_data.py}.
+
+"""
+
 import textwrap
 import re
-
-# You'll need these:
-# http://www.unicode.org/Public/3.1-Update/UnicodeData-3.1.0.txt
-# http://www.unicode.org/Public/3.1-Update/Blocks-4.txt
 
 def condenseCodepoints (codepoints):
     ranges = []
