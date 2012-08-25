2a40a644f853b2513b148ea06256fbfa613c73fa
Peter A. Bigot <pab@pabigot.com>
2012-08-25 18:54:10 -0500

    Update copyright span

--- pyxb/binding/content.py
+++ pyxb/binding/content.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2009-2011, Peter A. Bigot
+# Copyright 2009-2012, Peter A. Bigot
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain a
--- pyxb/bundles/wssplat/wsdl11.py
+++ pyxb/bundles/wssplat/wsdl11.py
@@ -1,4 +1,4 @@
-# Copyright 2009, Peter A. Bigot
+# Copyright 2009-2012, Peter A. Bigot
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain a
