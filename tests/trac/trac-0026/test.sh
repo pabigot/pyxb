b954d7d6fa52bcad6069f15fec730094a41811d4
Peter A. Bigot <pab@pabigot.com>
2012-11-05 07:07:16 -0600

    tess/trac-0026: rearrange to split exception hierarchy

--- tests/trac/trac-0026/test.sh
+++ tests/trac/trac-0026/test.sh
@@ -7,4 +7,7 @@ fail () {
 
 rm -f diagnostics.py*
 pyxbgen \
-   -u trac26.xsd -m trac26
+   -u trac26.xsd -m trac26 \
+|| fail Unable to build bindings
+python check-binding.py || fail BindingError check failed
+python check-validation.py || fail BindingError check failed
