f83bc642b65ea1072bc05cf990474376062bd4b3
Peter A. Bigot <pab@pabigot.com>
2013-02-28 06:09:52 -0600

    test-trac-0191: do not emit code.py

--- tests/trac/test-trac-0191.py
+++ tests/trac/test-trac-0191.py
@@ -21,7 +21,7 @@ xsd='''<?xml version="1.0" encoding="UTF-8"?>
 
 #file('schema.xsd', 'w').write(xsd)
 code = pyxb.binding.generate.GeneratePython(schema_text=xsd)
-file('code.py', 'w').write(code)
+#file('code.py', 'w').write(code)
 
 rv = compile(code, 'test', 'exec')
 eval(rv)
