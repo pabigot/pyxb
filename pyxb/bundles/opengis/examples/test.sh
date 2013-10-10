81c6c90a5795f0093fb20f461e9815209bd8f595
Peter A. Bigot <pab@pabigot.com>
2013-10-10 06:29:41 -0500

    opengis: add oseo 1.0 and wps 1.0.0 bindings

--- pyxb/bundles/opengis/examples/test.sh
+++ pyxb/bundles/opengis/examples/test.sh
@@ -17,6 +17,17 @@ if test -d "${SCHEMAS_OPENGIS_NET}" ; then
     | grep -v sosRegisterSensor \
     | xargs python check_sos.py \
   || exit 1
+  # The SOAP versions are irrelevant for this purpose;
+  # 53_wpsExecute_request_ComplexValue references a custom binding
+  # that hasn't been built because it isn't in OpenGIS.
+  ls ${SCHEMAS_OPENGIS_NET}/wps/1.0.0/examples/*.xml \
+    | sed -e '/SOAP/d' -e '/^53_/d' \
+    | xargs python check_wps.py \
+  || exit 1
+  ls ${SCHEMAS_OPENGIS_NET}/oseo/1.0/SampleMessages/*.xml \
+    | xargs python check_oseo.py \
+  || exit 1
+
 else
   echo 1>&2 "WARNING: Need SCHEMAS_OPENGIS_NET defined to test example documents"
 fi
