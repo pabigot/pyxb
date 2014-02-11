a30692bf92faf4c1ebc3cf1a39d2f0b90b6ab8d8
Peter A. Bigot <pab@pabigot.com>
2014-02-11 08:19:47 -0600

    test-trac-0091: extend with new test

--- tests/trac/test-trac-0091.py
+++ tests/trac/test-trac-0091.py
@@ -17,7 +17,14 @@ xsd='''<?xml version="1.0" encoding="UTF-8"?>
       <xs:fractionDigits value="8"/>
     </xs:restriction>
   </xs:simpleType>
-  <xs:element name="fv" type="f13p8"/>
+  <xs:element name="e13p8" type="f13p8"/>
+  <xs:simpleType name="f15p5">
+    <xs:restriction base="xs:decimal">
+      <xs:totalDigits value="15"/>
+      <xs:fractionDigits value="5"/>
+    </xs:restriction>
+  </xs:simpleType>
+  <xs:element name="e15p5" type="f15p5"/>
 </xs:schema>'''
 
 code = pyxb.binding.generate.GeneratePython(schema_text=xsd)
@@ -44,17 +51,32 @@ class TestTrac_0091 (unittest.TestCase):
     def testBasic (self):
         if sys.version_info[:2] >= (2, 7):
             # Prior to 2.7 float/Decimal comparisons returned invalid results.
-            self.assertEqual(1.0, fv(1.0))
-            self.assertEqual(1.0, fv('1.0'))
-            self.assertEqual(1234567890123.0, fv('1234567890123'))
-            self.assertEqual(1234567890123.0, CreateFromDocument('<fv>1234567890123</fv>'))
-        self.assertRaises(SimpleFacetValueError, fv, '12345678901234')
-        self.assertAlmostEqual(1.00000001, fv('1.00000001'))
-        self.assertRaises(SimpleFacetValueError, fv, '1.000000001')
+            self.assertEqual(1.0, e13p8(1.0))
+            self.assertEqual(1.0, e13p8('1.0'))
+            self.assertEqual(1234567890123.0, e13p8('1234567890123'))
+            self.assertEqual(1234567890123.0, CreateFromDocument('<e13p8>1234567890123</e13p8>'))
+        self.assertRaises(SimpleFacetValueError, e13p8, '12345678901234')
+        self.assertAlmostEqual(1.00000001, e13p8('1.00000001'))
+        self.assertRaises(SimpleFacetValueError, e13p8, '1.000000001')
 
     def testBadCase (self):
         # Prior to fix, this raised a facet violation due to rounding
-        self.assertAlmostEqual(0.00790287, fv('0.00790287'))
+        self.assertAlmostEqual(0.00790287, e13p8('0.00790287'))
+
+    def test15_5 (self):
+        from decimal import Decimal
+        # For compatibility in Python 2.6 PyXB will convert the float
+        # to a string before invoking the underlying decimal.Decimal
+        fv = e15p5(1234.56789)
+        self.assertTrue(fv.validateBinding())
+        dv = Decimal('1234.56789')
+        self.assertEqual(fv, dv)
+        # In Python 2.7 decimal.Decimal will create from the float,
+        # which will already be in a form that won't validate
+        self.assertRaises(SimpleFacetValueError, e15p5, Decimal(1234.56789))
+        sv = e15p5('1234.56789')
+        self.assertEqual(sv, dv)
+        self.assertTrue(sv.validateBinding())
 
 if __name__ == '__main__':
     unittest.main()
