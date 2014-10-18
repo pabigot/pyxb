9de2bf34e37eac6164ceb676e5678475b478ba27
Peter A. Bigot <pab@pabigot.com>
2014-10-18 12:10:48 -0500

    trac-0230: substitute open for obsolete function file

--- tests/trac/trac-0230/check.py
+++ tests/trac/trac-0230/check.py
@@ -1,5 +1,5 @@
 import pyxb
 import spirit
 
-instance = spirit.CreateFromDocument(file('sample.xml').read())
+instance = spirit.CreateFromDocument(open('sample.xml').read())
 assert 'generated' == instance.busInterfaces.busInterface[1].mirroredSlave.baseAddresses.remapAddress[0].resolve
