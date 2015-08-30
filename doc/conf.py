e1c939662bfae28045838ac383e66f352324adbb
Peter A. Bigot <pab@pabigot.com>
2015-08-30 07:29:25 -0500

    passim: regenerate version/year-dependent markings

--- doc/conf.py
+++ doc/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'PyXB'
-copyright = '2009-2014, Peter A. Bigot'
+copyright = '2009-2015, Peter A. Bigot'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
