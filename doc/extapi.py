69be38abacdb3494cd8bde08f0f0f4ecb295ec67
Peter A. Bigot <pab@pabigot.com>
2017-05-13 07:35:10 -0500

    doc/extapi: remove GPL material
    http://git.savannah.gnu.org/cgit/kenozooid.git/tree/doc/extapi.py was
    originally adopted to link from sphinx to to epydoc-generated
    documentation.  epydoc was last updated over nine years ago, so it's
    dead.  A downstream packager noticed that the imported material was GPL,
    being the only part of PyXB so licensed and conflicting with the
    intended Apache 2.0 license.
    
    Remove the material that came from the original file, leaving only the
    ticket-related roles that were subsequently added for PyXB support.
    Mark that material as Apache 2.0.
    
    Closes #77.

--- doc/extapi.py
+++ doc/extapi.py
@@ -1,22 +1,17 @@
-# Kenozooid - software stack to support different capabilities of dive
-# computers.
+# -*- coding: utf-8 -*-
+# Copyright 2009-2017, Peter A. Bigot
 #
-# Copyright (C) 2009 by Artur Wroblewski &lt;wrobell@pld-linux.org&gt;
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain a
+# copy of the License at:
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#            http://www.apache.org/licenses/LICENSE-2.0
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
-#
-# Modifications by PAB to override reference text
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
 from __future__ import print_function
 import os.path
@@ -25,74 +20,6 @@ import sys
 import re
 
 __Reference_re = re.compile('\s*(.*)\s+<(.*)>\s*$', re.MULTILINE + re.DOTALL)
-
-def api_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
-    """
-    Role `:api:` bridges generated API documentation by tool like EpyDoc
-    with Sphinx Python Documentation Generator.
-
-    Other tools, other than EpyDoc, can be easily supported as well.
-
-    First generate the documentation to be referenced, i.e. with EpyDoc::
-
-        $ mkdir -p doc/html/api
-        $ epydoc -o doc/html/api ...
-
-    Next step is to generate documentation with Sphinx::
-
-        $ sphinx-build doc doc/html
-
-    """
-    basedir = 'api'
-    prefix = 'html/' # fixme: fetch it from configuration
-    exists = lambda f: os.path.exists(prefix + f)
-
-    # assume module is references
-
-    #print 'Text "%s"' % (text,)
-    mo = __Reference_re.match(text)
-    label = None
-    if mo is not None:
-        ( label, text ) = mo.group(1, 2)
-    name = text.strip()
-
-    uri = file = '%s/%s-module.html' % (basedir, text)
-    chunks = text.split('.')
-
-    #print 'Trying module file %s' % (file,)
-
-    # if not module, then a class
-    if not exists(file):
-        name = text.split('.')[-1]
-        uri = file = '%s/%s-class.html' % (basedir, text)
-    #print 'Trying class file %s' % (file,)
-
-    # if not a class, then function or class method
-    if not exists(file):
-        method = chunks[-1]
-        fprefix = '.'.join(chunks[:-1])
-        # assume function is referenced
-        file = '%s/%s-module.html' % (basedir, fprefix)
-        #print 'Trying method file %s' % (file,)
-        if exists(file):
-            uri = '%s#%s' % (file, method)
-        else:
-            # class method is references
-            file = '%s/%s-class.html' % (basedir, fprefix)
-            if exists(file):
-                name = '.'.join(chunks[-2:]) # name should be Class.method
-                uri = '%s/%s-class.html#%s' % (basedir, fprefix, method)
-
-    if label is None:
-        label = name
-    if exists(file):
-        node = nodes.reference(rawtext, label, refuri=uri, **options)
-    else:
-        # cannot find reference, then just inline the text
-        print('WARNING: Unable to find %s in API' % (text,))
-        node = nodes.literal(rawtext, text)
-
-    return [node], []
 
 def ticket_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
     """
@@ -137,31 +64,6 @@ def issue_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
 
     return [node], []
 
-
-def pyex_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
-    """
-    Role `:pyex:` generates reference to Python exception classes.
-    """
-
-    pyex_fmt = 'http://docs.python.org/library/exceptions.html#exceptions.%s'
-    mo = __Reference_re.match(text)
-    label = None
-    if mo is not None:
-        ( label, text ) = mo.group(1, 2)
-    exc = text.strip()
-    print('Python exception %s as %s' % (text, label))
-
-    uri = pyex_fmt % (exc,)
-    if label is None:
-        label = '%s' % (exc,)
-    node = nodes.reference(rawtext, label, refuri=uri, **options)
-
-    return [node], []
-
 def setup(app):
-    app.add_role('api', api_role)
-    app.add_config_value('epydoc_basedir', 'api', False)
-    app.add_config_value('epydoc_prefix', 'doc/html/', False)
     app.add_role('ticket', ticket_role)
     app.add_role('issue', issue_role)
-    app.add_role('pyex', pyex_role)
