92db87f9ce94c7e4d0a5a3c82a6bb602b75aba49
Peter A. Bigot <pab@pabigot.com>
2017-05-20 09:17:32 -0500

    test/trac-0193: correct test
    The original solution fixed the exception when bindings were generated,
    but the test passed only because of issue #78 which took four years more
    to discover.  Revise the test to more accurately reflect what the
    original fix enabled (and demonstrate the error in issue #78).
    
    https://sourceforge.net/p/pyxb/tickets/193/

--- tests/trac/trac-0193/check.py
+++ tests/trac/trac-0193/check.py
@@ -1,6 +1,14 @@
 import trac193
+import pyxb.binding.datatypes as xs
 
 bdv = trac193.Money._UseForTag('Value').elementBinding().typeDefinition()
+assert bdv == xs.integer
+assert trac193.Money._UseForTag('Currency').elementBinding().typeDefinition() == xs.anySimpleType
+assert trac193.Money._UseForTag('Season').elementBinding().typeDefinition() == xs.anyType
+
 rdv = trac193.OpenDeliveries._UseForTag('Value').elementBinding().typeDefinition()
+assert rdv == xs.integer
+assert trac193.OpenDeliveries._UseForTag('Currency').elementBinding().typeDefinition() == xs.anySimpleType
+assert trac193.OpenDeliveries._UseForTag('Season').elementBinding().typeDefinition() == xs.anyType
 
 assert issubclass(rdv, bdv)
