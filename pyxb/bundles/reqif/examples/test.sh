cde44879e773cbd9ee2978eb76eeeb358ecff913
Peter A. Bigot <pab@pabigot.com>
2017-08-04 16:20:02 -0500

    reqif: add bundle
    This is an interesting-looking definition for requirements interchange,
    which caused trouble since it uses a modularized XHTML definition that
    conflicts with what PyXB normally uses.  Add material to build it and
    verify that it basically works.
    
    Closes #81
    See: http://www.omg.org/spec/ReqIF/1.2/

---
+++ pyxb/bundles/reqif/examples/test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. ${PYXB_ROOT}/tests/support.sh
+
+TEST_URI=https://raw.githubusercontent.com/redsteve/EnterpriseArchitect_ReqIF_AddIn/master/EA_ReqIF_AddIn/example.reqif.xml
+
+if [ ! -f in.xml ]; then
+  wget -O in.xml ${TEST_URI} || fail retrieving document
+fi
+
+PYXB_ARCHIVE_PATH=${PYXB_ROOT}/pyxb/bundles/reqif//:+
+export PYXB_ARCHIVE_PATH
+
+python process.py > test.out || fail running
+cmp test.out test.expected || fail output comparison
+passed
