01b9bc1dfdc647c3a344153c1d4e979c66b1d5fd
Peter A. Bigot <pab@pabigot.com>
2017-09-02 06:39:53 -0500

    examples: correct reference to fail function

--- examples/content/test.sh
+++ examples/content/test.sh
@@ -5,7 +5,7 @@
 rm -f content.py
 pyxbgen \
     -u content.xsd -m content \
-    || failed generating bindings
+    || fail generating bindings
 python showcontent.py > test.out || fail running
 cmp test.out test.expected || fail output comparison
 passed
--- examples/dictionary/test.sh
+++ examples/dictionary/test.sh
@@ -2,7 +2,7 @@
 
 . ${PYXB_ROOT}/tests/support.sh
 
-sh genbindings.sh || failed generating bindings
-python showdict.py > showdict.out || failed running
-cmp showdict.out showdict.expected || failed output comparison
+sh genbindings.sh || fail generating bindings
+python showdict.py > showdict.out || fail running
+cmp showdict.out showdict.expected || fail output comparison
 passed
